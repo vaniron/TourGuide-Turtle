2c364650c598c3301a8736dd77add1fb3570bd55
kyliexx <kyliebuckler.fx@gmail.com>
2022-09-28 21:29:13 +0930

    Updates

--- TourGuide_Alliance/30_31_Wetlands.lua
+++ TourGuide_Alliance/30_31_Wetlands.lua
@@ -1,73 +1,67 @@
-local Guide = DugisGuideViewer:RegisterModule("DugisGuide_Leveling_Alliance_En_30_31_Wetlands")
-function Guide:Initialize()
-	function Guide:Load()DugisGuideViewer:RegisterGuide("|cffffd200Azeroth 30-40|r", "1437(30-31)#1437(30-31)#1437(30-31)", "1424(31-32)#1424(31-32)#1424(31-32)", "Alliance", nil, "L", nil, function()
+TourGuide:RegisterGuide("Wetlands (30-31)", "Hillsbrad Foothills (31-32)", "Alliance", function()
 return [[
 
-A Seeking Strahad |QID|1798| |N|(npc:6120) in {Ironforge} (47.2, 10.9)| |C|Warlock| |Z|1455| |NPC|6120|
-A The Islander  |QID|1718| |N|(npc:5113) in {Ironforge} (71.5, 91.2)| |C|Warrior| |Z|1455| |NPC|5113|
+A Seeking Strahad |QID|1798| |N|Lago Blackwrench in {Ironforge} (47.2, 10.9)| |C|Warlock|
+A The Islander  |QID|1718| |N|Kelv Sternhammer in {Ironforge} (71.5, 91.2)| |C|Warrior|
 
-R Fray Island |QID|1719| |N|Travel to {Fray Island} (68.6, 49.1)| |Z|1413| |C|Warrior|
-T The Islander |QID|1718| |N|(npc:6236) in {Fray Island} (68.6, 49.1)| |Z|1413| |C|Warrior| |NPC|6236|
-A The Affray |QID|1719| |N|(npc:6236) in {Fray Island} (68.6, 49.1)| |Z|1413| |C|Warrior| |NPC|6236|
-C The Affray |QID|1719| |N|Kill the series of Mobs, bandage and eat in between Mobs. As for (npc:6238), you're given the privilege to attack him first because he's neutral, so just run back and (spell:100) then use your (spell:20230) for (npc:6238) (68.6, 48.7)| |Z|1413| |C|Warrior| |NPC|6236|
-T The Affray |QID|1719| |N|(npc:6236) in {Fray Island} (68.6, 49.1)| |Z|1413| |C|Warrior| |NPC|6236|
+R Fray Island |QID|1719| |N|Travel to {Fray Island} (68.6, 49.1)|   |C|Warrior|
+T The Islander |QID|1718| |N|Klannoc Macleod in {Fray Island} (68.6, 49.1)|   |C|Warrior|
+A The Affray |QID|1719| |N|Klannoc Macleod in {Fray Island} (68.6, 49.1)|   |C|Warrior|
+C The Affray |QID|1719| |N|Kill the series of Mobs, bandage and eat in between Mobs. As for Big Will, you're given the privilege to attack him first because he's neutral, so just run back and Charge then use your Retaliation for Big Will (68.6, 48.7)|   |C|Warrior|
+T The Affray |QID|1719| |N|Klannoc Macleod in {Fray Island} (68.6, 49.1)|   |C|Warrior|
 
-T Seeking Strahad |QID|1798| |N|(npc:6251) in {Ratchet} (62.6, 35.5)| |Z|1413| |C|Warlock| |NPC|6251|
-A Tome of the Cabal (1) |QID|1758| |N|(npc:6251) in {Ratchet} (62.6, 35.5)| |Z|1413| |C|Warlock| |NPC|6251|
+T Seeking Strahad |QID|1798| |N|Strahad Farsan in {Ratchet} (62.6, 35.5)|   |C|Warlock|
+A Tome of the Cabal (1) |QID|1758| |N|Strahad Farsan in {Ratchet} (62.6, 35.5)|   |C|Warlock|
 
-T Tome of the Cabal (1) |QID|1758| |N|(npc:6294) in {Ironforge} (74.3, 9.4)| |C|Warlock| |Z|1455| |NPC|6294|
-A Tome of the Cabal (2) |QID|1802| |N|(npc:6294) in {Ironforge} (74.3, 9.4)| |C|Warlock| |Z|1455| |NPC|6294|
+T Tome of the Cabal (1) |QID|1758| |N|Krom Stoutarm in {Ironforge} (74.3, 9.4)| |C|Warlock|
+A Tome of the Cabal (2) |QID|1802| |N|Krom Stoutarm in {Ironforge} (74.3, 9.4)| |C|Warlock|
 
-N (item:6997) |QID|1802| |N|Collect (item:6997) inside the cave east of the Great Lift in Thousand Needles (43.5, 32.7)| |L|6997| |Z|1441| |C|Warlock|
+N Tattered Manuscript |QID|1802| |N|Collect Tattered Manuscript inside the cave east of the Great Lift in Thousand Needles (43.5, 32.7)| |L|6997|   |C|Warlock|
 
-C Tome of the Cabal (2) |QID|1802| |N|Collect the (item:6931) (27.7, 72.8)| |L|6931| |C|Warlock| |Z|1424|
+C Tome of the Cabal (2) |QID|1802| |N|Collect the Moldy Tome (27.7, 72.8)| |L|6931| |C|Warlock|
 
-T Tome of the Cabal (2) |QID|1802| |N|(npc:6294) in {Ironforge} (74.3, 9.8)| |C|Warlock| |Z|1455|
-A Tome of the Cabal (3) |QID|1804| |N|(npc:6294) in {Ironforge} (74.3, 9.8)| |C|Warlock| |Z|1455|
---A In Search of Menara Voidrender |QID|4736| |N|Briarthorn (50.3, 6.1)| |C|Warlock| |Z|1455|
+T Tome of the Cabal (2) |QID|1802| |N|Krom Stoutarm in {Ironforge} (74.3, 9.8)| |C|Warlock|
+A Tome of the Cabal (3) |QID|1804| |N|Krom Stoutarm in {Ironforge} (74.3, 9.8)| |C|Warlock|
+--A In Search of Menara Voidrender |QID|4736| |N|Briarthorn (50.3, 6.1)| |C|Warlock|
 
-C Tome of the Cabal (3) |QID|1804| |N|Kill (npc:1057) and (npc:1038) to collect 3 (item:6930) (49, 46)| |C|Warlock| |Z|1437| |W| |NPC|1057, 1038|
+C Tome of the Cabal (3) |QID|1804| |N|Kill Dragonmaw Bonewarder and Dragonmaw Shadowwarder to collect 3 Rod of Channeling (49, 46)| |C|Warlock|
 
-T Tome of the Cabal (3) |QID|1804| |N|(npc:6251) in {Ratchet} (62.6, 35.5)| |C|Warlock| |Z|1413| |NPC|6251|
-A The Binding |QID|1795| |N|(npc:6251) in {Ratchet} (62.6, 35.5)| |Z|1413| |C|Warlock| |NPC|6251|
-C The Binding |QID|1795| |N|Using the (item:6999), summon and subdue a (npc:6268) (62.6, 35.3)| |U|6999| |Z|1413| |C|Warlock| |NPC|6268|
-T The Binding  |QID|1795| |N|(npc:6251) in {Ratchet} (62.6, 35.5)| |Z|1413| |C|Warlock| |QID|1795| |NPC|6251|
+T Tome of the Cabal (3) |QID|1804| |N|Strahad Farsan in {Ratchet} (62.6, 35.5)| |C|Warlock|
+A The Binding |QID|1795| |N|Strahad Farsan in {Ratchet} (62.6, 35.5)|   |C|Warlock|
+C The Binding |QID|1795| |N|Using the Tome of the Cabal, summon and subdue a Summoned Felhunter (62.6, 35.3)| |U|6999|   |C|Warlock|
+T The Binding  |QID|1795| |N|Strahad Farsan in {Ratchet} (62.6, 35.5)|   |C|Warlock| |QID|1795|
 
-R Deepwater Tavern |QID|288| |N|Travel to {Deepwater Tavern} (10.68, 60.92)| |Z|1437|
-h Deepwater Tavern |QID|288| |N|Speak to (npc:1464) and set hearth at {Deepwater Tavern} (10.7, 60.9)| |NPC|1464|
-A The Third Fleet |QID|288| |N|(npc:1239) in {Menethil Harbor} (10.88, 59.64)| |Z|1437| |NPC|1239|
-B (item:2594) |QID|288| |N|Purchase (item:2594) from (npc:1464) in {Deepwater Tavern} (10.69, 60.89)| |Z|1437|
-T The Third Fleet |QID|288| |N|(npc:1239) in {Menethil Harbor} (10.88, 59.64)| |Z|1437| |NPC|1239|
-A The Cursed Crew |QID|289| |N|(npc:1239) in {Menethil Harbor} (10.91, 59.53)| |Z|1437| |NPC|1239|
+R Deepwater Tavern |QID|288| |N|Travel to {Deepwater Tavern} (10.68, 60.92)|
+h Deepwater Tavern |QID|288| |N|Speak to Innkeeper Helbrek and set hearth at {Deepwater Tavern} (10.7, 60.9)|
+A The Third Fleet |QID|288| |N|First Mate Fitzsimmons in {Menethil Harbor} (10.88, 59.64)|
+B [Flagon of Mead] |QID|288| |N|Purchase Flagon of Mead from Innkeeper Helbrek in {Deepwater Tavern} (10.69, 60.89)|
+T The Third Fleet |QID|288| |N|First Mate Fitzsimmons in {Menethil Harbor} (10.88, 59.64)|
+A The Cursed Crew |QID|289| |N|First Mate Fitzsimmons in {Menethil Harbor} (10.91, 59.53)|
 
-R The Lost Fleet |QID|289| |N|Travel to {The Lost Fleet} (13.91, 31.03)| |Z|1437|
-K (npc:1159) |QID|289.3| |N|Collect (item:3619) in {The Lost Fleet} (13.91, 31.03)| |Z|1437| |NPC|1159|
-C The Cursed Crew |QID|289| |N|Kill 13 (npc:1157) and 5 (npc:1158) which are found around the shipwreck (13, 27) (14, 30)| |NPC|1157, 1158|
-T The Cursed Crew |QID|289| |N|(npc:1239) in {Menethil Harbor} (10.91, 59.53)| |Z|1437| |NPC|1239|
-A Lifting the Curse |QID|290| |N|(npc:1239) in {Menethil Harbor} (10.91, 59.53)| |Z|1437| |NPC|1239|
+R The Lost Fleet |QID|289| |N|Travel to {The Lost Fleet} (13.91, 31.03)|  
+K First Mate Snellig |QID|289.3| |N|Collect Snellig's Snuffbox in {The Lost Fleet} (13.91, 31.03)|
+C The Cursed Crew |QID|289| |N|Kill 13 Cursed Sailor and 5 Cursed Marine which are found around the shipwreck (14, 30)|
+T The Cursed Crew |QID|289| |N|First Mate Fitzsimmons in {Menethil Harbor} (10.91, 59.53)|
+A Lifting the Curse |QID|290| |N|First Mate Fitzsimmons in {Menethil Harbor} (10.91, 59.53)|
 
-R The Lost Fleet |QID|290| | |N|Travel to {The Lost Fleet} (15.20, 23.63)| |Z|1437|
-C Lifting the Curse |QID|290| |N|Kill (npc:1160) for the (item:2629). Walk up the rudder on the top floor to reach him (15.5, 23.5)| |NPC|1160|
+R The Lost Fleet |QID|290| | |N|Travel to {The Lost Fleet} (15.20, 23.63)|
+C Lifting the Curse |QID|290| |N|Kill Captain Halyndor for the Intrepid Strongbox Key. Walk up the rudder on the top floor to reach him (15.5, 23.5)|
 T Lifting the Curse |QID|290| |N|Intrepid's Locked Strongbox (14.5, 24.1) Open the box on bottom floor of the boat, it's underwater |
 A The Eye of Paleth |QID|292| |N|Intrepid's Locked Strongbox (14.5, 24.1)|
 
 R Angerfang Encampment |TID|465| |N|Travel to {Angerfang Encampment} (47.5, 46.9)| |O|
 T Nek'rosh's Gambit |QID|465| |N|Dragonmaw Catapult in {Angerfang Encampment} (47.5, 46.9)| |O|
-A Defeat Nek'rosh |QID|474| |N|Dragonmaw Catapult in {Angerfang Encampment} (47.40, 46.90)| |Z|1437| |PRE|465|
-C Defeat Nek'rosh |QID|474| |N|Kill (npc:2091) in {Angerfang Encampment}<br/><br/>(npc:2091) is a level 32 elite npc and difficult to solo, you can give it a try if you're good, the graveyard is nearby otherwise safe to skip (53.50, 54.67)| |Z|1437| |PRE|465|
+A Defeat Nek'rosh |QID|474| |N|Dragonmaw Catapult in {Angerfang Encampment} (47.40, 46.90)|   |PRE|465|
+C Defeat Nek'rosh |QID|474| |N|Kill Chieftain Nek'rosh in {Angerfang Encampment} Chieftain Nek'rosh is a level 32 elite npc and difficult to solo, you can give it a try if you're good, the graveyard is nearby otherwise safe to skip (53.50, 54.67)|   |PRE|465|
 
-R Deepwater Tavern |OID|293| |N|Travel or (item:6948) to {Deepwater Tavern} (10.60, 60.61)| |Z|1437|
-T The Eye of Paleth |QID|292| |N|(npc:1217) in {Deepwater Tavern} (10.60, 60.61)| |Z|1437| |NPC|1217|
-A Cleansing the Eye |QID|293| |N|(npc:1217) in {Deepwater Tavern} (10.60, 60.61)| |Z|1437| |NPC|1217|
+R Deepwater Tavern |OID|293| |N|Travel or Hearthstone to {Deepwater Tavern} (10.60, 60.61)|
+T The Eye of Paleth |QID|292| |N|Glorin Steelbrow in {Deepwater Tavern} (10.60, 60.61)|
+A Cleansing the Eye |QID|293| |N|Glorin Steelbrow in {Deepwater Tavern} (10.60, 60.61)|
 
-R Menethil Keep |TID|474| |N|Travel to {Menethil Keep} (9.90, 57.45)| |Z|1437|
-T Defeat Nek'rosh |QID|474| |N|(npc:2104) in {Menethil Keep} (9.90, 57.45)| |Z|1437| |PRE|465| |NPC|2104|
+R Menethil Keep |TID|474| |N|Travel to {Menethil Keep} (9.90, 57.45)|
+T Defeat Nek'rosh |QID|474| |N|Captain Stoutfist in {Menethil Keep} (9.90, 57.45)|   |PRE|465|
 
 N Guide Complete |N|Tick to continue to the next guide|
 
 ]]
-end, {image = "wetlands.tga", description = [[]]})	end
-
-	function Guide:Unload()
-	end
-end
+end)
