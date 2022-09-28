2c194843f5ae8f423df216a1a59d4b51fe720446
jb6357 <justin.boencke@fujifilm.kj@fujifilm.com>
2022-09-28 11:19:37 +0930

    new version

--- TourGuide_Alliance/21_22_Ashenvale.lua
+++ TourGuide_Alliance/21_22_Ashenvale.lua
@@ -1,81 +1,73 @@
-local Guide = DugisGuideViewer:RegisterModule("DugisGuide_Leveling_Alliance_En_21_22_Ashenvale")
-function Guide:Initialize()
-	function Guide:Load()DugisGuideViewer:RegisterGuide("|cffffd200Azeroth 20-30|r", "1440(21-22)#1440(21-22)#1440(21-22)", "1442(22-23)#1442(22-23)#1442(22-23)", "Alliance", nil, "L", nil, function()
+TourGuide:RegisterGuide("Ashenvale (21-22)", "Stonetalon Mountains (22-23)", "Alliance",function()
 return [[
 
-R Maestra's Post |QID|970| |N|Travel to {Maestra's Post} (27.25, 35.62)| |Z|1440|
-T The Sleeper Has Awakened |QID|5321| |N|(npc:11219) in {Maestra's Post} (27.25, 35.62)| |Z|1440| |O| |NPC|11219|
-T The Tower of Althalaxx (3) |QID|967| |N|(npc:3663) in {Maestra's Post} (26.20, 38.66)| |Z|1440| |O| |NPC|3663| 
-A The Tower of Althalaxx (4) |QID|970| |N|(npc:3663) in {Maestra's Post} (26.20, 38.66)| |Z|1440| |PRE|970| |NPC|3663|
-A Bathran's Hair |QID|1010| |N|(npc:3847) in {Maestra's Post} (26.42, 38.56)| |Z|1440| |NPC|3847|
+R Maestra's Post |QID|970| |N|Travel to Maestra's Post (27.25, 35.62)|
+T The Sleeper Has Awakened |QID|5321| |N|Liladris Moonriver in Maestra's Post (27.25, 35.62)|  |O|
+T The Tower of Althalaxx (3) |QID|967| |N|Delgren in Maestra's Post (26.20, 38.66)|  |O|
+A The Tower of Althalaxx (4) |QID|970| |N|Delgren in Maestra's Post (26.20, 38.66)|  |PRE|970|
+A Bathran's Hair |QID|1010| |N|Orendil Broadleaf in Maestra's Post (26.42, 38.56)|
 
-C The Tower of Althalaxx (4) |QID|970| |N|Kill (npc:3728), (npc:3727) or (npc:3725) and collect a (item:5366) (32, 30)| |W| |PRE|970| |NPC|3728, 3727, 3725|
-C Bathran's Hair |QID|1010| |N|Collect 5 of (item:5437) found in the plant bundles on the ground in the camp (31, 22)| |W|
+C The Tower of Althalaxx (4) |QID|970| |N|Kill mobs, will likely take a lot of time and collect a [Glowing Soul Gem] (32, 30)|
+C Bathran's Hair |QID|1010| |N|Collect 5 of [Bathran's Hair] found in the plant bundles on the ground in the camp (31, 22)|
 
-R Maestra's Post |QID|973| |N|Travel to {Maestra's Post} (26.20, 38.66)| |Z|1440| 
-T The Tower of Althalaxx (4) |QID|970| |N|(npc:3663) in {Maestra's Post} (26.20, 38.66)| |Z|1440| |PRE|970| |NPC|3663|
-A The Tower of Althalaxx (5) |QID|973| |N|(npc:3663) in {Maestra's Post} (26.20, 38.66)| |Z|1440| |PRE|970| |NPC|3663|
-T Bathran's Hair |QID|1010| |N|(npc:3847) in {Maestra's Post} (26.42, 38.56)| |Z|1440| |NPC|3847|
-A Orendil's Cure |QID|1020| |N|(npc:3847) in {Maestra's Post} (26.42, 38.56)| |Z|1440| |NPC|3847|
+R Maestra's Post |QID|973| |N|Travel to Maestra's Post (26.20, 38.66)|
+T The Tower of Althalaxx (4) |QID|970| |N|Delgren in Maestra's Post (26.20, 38.66)|  |PRE|970|
+A The Tower of Althalaxx (5) |QID|973| |N|Delgren in Maestra's Post (26.20, 38.66)|  |PRE|970|
+T Bathran's Hair |QID|1010| |N|Orendil Broadleaf in Maestra's Post (26.42, 38.56)|
+A Orendil's Cure |QID|1020| |N|Orendil Broadleaf in Maestra's Post (26.42, 38.56)|
 
-R The Shrine of Aessina |TID|945| |N|Travel to {The Shrine of Aessina} (22.69, 51.89)| |Z|1440| |O| |NPC|3585|
-T Therylune's Escape |QID|945| |N|(npc:3585) in {The Shrine of Aessina} (22.69, 51.89)| |Z|1440| |O| |NPC|3585|
+R The Shrine of Aessina |TID|945| |N|Travel to The Shrine of Aessina (22.69, 51.89)|  |O|
+T Therylune's Escape |QID|945| |N|Therysil in The Shrine of Aessina (22.69, 51.89)|  |O|
 
-R Astranaar |QID|1008| |N|Travel to {Astranaar} (34.5, 48)|
-f Astranaar |QID|1008| |N|Speak to (npc:4267) and grab flight path for {Astranaar} (34.5, 48)| |NPC|4267|
-A The Zoram Strand |QID|1008| |N|(npc:3845) in {Astranaar} (34.65, 48.88)| |Z|1440| |NPC|3845|
-A On Guard in Stonetalon |QID|1070| |N|(npc:4079) in {Astranaar} (34.92, 49.77)| |Z|1440| |NPC|4079|
-A Journey to Stonetalon Peak |QID|1056| |N|(npc:3996) in {Astranaar} (35.78, 49.16)| |Z|1440| |NPC|3996|
-A Raene's Cleansing (1) |QID|991| |N|(npc:3691) in {Astranaar} (36.59, 49.59)| |Z|1440| |NPC|3691|
-A Culling the Threat |QID|1054| |N|(npc:3691) in {Astranaar} (36.59, 49.63)| |Z|1440| |NPC|3691|
-h Astranaar |QID|1033| |N|Speak to (npc:6738) and set hearth at {Astranaar} (37, 49.3)| |NPC|6738|
-T Orendil's Cure |QID|1020| |N|(npc:3894) in {Astranaar} (37.37, 51.78)| |Z|1440| |NPC|3894|
-A Elune's Tear |QID|1033| |N|(npc:3894) in {Astranaar} (37.37, 51.78)| |Z|1440| |NPC|3894|
-A The Ancient Statuette |QID|1007| |N|(npc:3846) in {} (14.81, 31.25)| |Z|1440| |NPC|3846|
+R Astranaar |QID|1008| |N|Travel to Astranaar (34.5, 48)|
+f Astranaar |QID|1008| |N|Grab flight path for Astranaar (34.5, 48)|
+A The Zoram Strand |QID|1008| |N|Shindrell Swiftfire in Astranaar (34.65, 48.88)|
+A On Guard in Stonetalon |QID|1070| |N|Sentinel Thenysil in Astranaar (34.92, 49.77)|
+A Journey to Stonetalon Peak |QID|1056| |N|Faldreas Goeth'Shael in Astranaar (35.78, 49.16)|
+A Raene's Cleansing (1) |QID|991| |N|Raene Wolfrunner in Astranaar (36.59, 49.59)|
+A Culling the Threat |QID|1054| |N|Raene Wolfrunner in Astranaar (36.59, 49.63)|
+h Astranaar |QID|1033| |N|Set hearth at Astranaar (37, 49.3)|
+T Orendil's Cure |QID|1020| |N|Pelturas Whitemoon in Astranaar (37.37, 51.78)|
+A Elune's Tear |QID|1033| |N|Pelturas Whitemoon in Astranaar (37.37, 51.78)|
+A The Ancient Statuette |QID|1007| |N|Talen (14.81, 31.25)|
 
-N As you go... |AYG|1009| |QID|1008| |N|Kill any nagas around {The Zoram Strand} and collect 20 (item:5490)|
-C The Ancient Statuette |QID|1007| |N|Collect the (item:5424) laying on the ground in {The Zoram Strand} (14.2, 20.6)| |W|
-T The Ancient Statuette |QID|1007| |N|(npc:3846) in {The Zoram Strand} (14.81, 31.25)| |Z|1440| |NPC|3846|
-A Ruuzel |QID|1009| |N|(npc:3846) in {The Zoram Strand} (14.81, 31.25)| |Z|1440| |NPC|3846|
-C Ruuzel |QID|1009| |N|Kill (npc:3943) collect the (item:5445)<br/><b>She patrols clockwise around the small island with two bodyguards (7, 14)| |W| |NPC|3943|
-C The Zoram Strand |QID|1008| |N|Kill any (npc:3713), (npc:3717) or (npc:3715) which surround {The Zoram Strand} until you've collected 20 (item:5490) (15, 24)| |W| |NPC|3713, 3717, 3715|
-T Ruuzel |QID|1009| |N|(npc:3846) in {The Zoram Strand} (14.81, 31.25)| |Z|1440| |NPC|3846|
+N As you go... |AYG|1009| |QID|1008| |N|Kill any nagas around The Zoram Strand and collect 20 [Wrathtail Head]|
+C The Ancient Statuette |QID|1007| |N|Collect the [Ancient Statuette] laying on the ground in The Zoram Strand (14.2, 20.6)|
+T The Ancient Statuette |QID|1007| |N|Talen in The Zoram Strand (14.81, 31.25)|
+A Ruuzel |QID|1009| |N|Talen in The Zoram Strand (14.81, 31.25)|
+C Ruuzel |QID|1009| |N|Kill Ruuzel collect the [Ring of Zoram]<br/><b>She patrols clockwise around the small island with two bodyguards<br/><b>Or there is a rare that drops the same(7, 14)|
+C The Zoram Strand |QID|1008| |N|Kill any nagas which surround The Zoram Strand until you've collected 20 [Wrathtail Head] (15, 24)|
+T Ruuzel |QID|1009| |N|Talen in The Zoram Strand (14.81, 31.25)|
 
-R Lake Falathim |QID|1023| |N|Travel to {Lake Falathim} (20.32, 42.39)| |Z|1440|
-T Raene's Cleansing (1) |QID|991| |N|(npc:3891) in {Lake Falathim} (20.32, 42.39)| |Z|1440| |NPC|3891|
-A Raene's Cleansing (2) |QID|1023| |N|(npc:3891) in {Lake Falathim} (20.32, 42.39)| |Z|1440| |NPC|3891|
-C Raene's Cleansing (2) |QID|1023| |N|Kill the (npc:3742), (npc:3737) or (npc:3739) at the nearby lake until you've collected the (item:5463) (19, 43)| |W| |NPC|3742, 3737, 3739|
+R Lake Falathim |QID|1023| |N|Travel to Lake Falathim (20.32, 42.39)|
+T Raene's Cleansing (1) |QID|991| |N|Teronis in Lake Falathim (20.32, 42.39)|
+A Raene's Cleansing (2) |QID|1023| |N|Teronis in Lake Falathim (20.32, 42.39)|
+C Raene's Cleansing (2) |QID|1023| |N|Kill the murlocs at the nearby lake until you've collected the [Glowing Gem] (19, 43)|
 
-R Astranaar |QID|1024| |N|Travel to {Astranaar} (36.59, 49.59)| |Z|1440|
-T Raene's Cleansing (2) |QID|1023| |N|(npc:3691) in {Astranaar} (36.59, 49.59)| |Z|1440| |NPC|3691|
-A Raene's Cleansing (3) |QID|1024| |N|(npc:3691) in {Astranaar} (36.59, 49.59)| |Z|1440| |NPC|3691|
-A An Aggressive Defense |QID|1025| |N|(npc:3691) in {Astranaar} (36.59, 49.59)| |Z|1440| |NPC|3691|
-T The Zoram Strand |QID|1008| |N|(npc:3845) in {Astranaar} (34.65, 48.88)| |Z|1440| |NPC|3845|
-A Pridewings of Stonetalon |QID|1134| |N|(npc:3845) in {Astranaar} (34.68, 48.88)| |Z|1440| |NPC|3845|
+R Astranaar |QID|1024| |N|Travel to Astranaar (36.59, 49.59)|
+T Raene's Cleansing (2) |QID|1023| |N|Raene Wolfrunner in Astranaar (36.59, 49.59)|
+A Raene's Cleansing (3) |QID|1024| |N|Raene Wolfrunner in Astranaar (36.59, 49.59)|
+A An Aggressive Defense |QID|1025| |N|Raene Wolfrunner in Astranaar (36.59, 49.59)|
+T The Zoram Strand |QID|1008| |N|Shindrell Swiftfire in Astranaar (34.65, 48.88)|
+A Pridewings of Stonetalon |QID|1134| |N|Shindrell Swiftfire in Astranaar (34.68, 48.88)|
 
-R Iris Lake |QID|1033| |N|Travel to {Iris Lake} (44.18, 56.29) (43.56, 49.23)| |Z|1440|
-C Elune's Tear |QID|1033| |N|Find and retrieve (item:5493) in {Iris Lake} (43.48, 45.76) (46.25, 45.93)| |Z|1440|
+R Iris Lake |QID|1033| |N|Travel to Iris Lake (44.18, 56.29) (43.56, 49.23)|
+C Elune's Tear |QID|1033| |N|Find and retrieve [Elune's Tear] in Iris Lake (43.48, 45.76) (46.25, 45.93)|
 
-R Moonwell |QID|1026| |N|Travel to {Moonwell} (48, 47) (53.49, 46.30)| |Z|1440|
-T Raene's Cleansing (3) |QID|1024| |N|(npc:3916) in {Moonwell} (53.49, 46.30)| |Z|1440| |NPC|3916|
-A Raene's Cleansing (4) |QID|1026| |N|(npc:3916) in {Moonwell} (53.49, 46.30)| |Z|1440| |NPC|3916|
+R Moonwell |QID|1026| |N|Travel to Moonwell (48, 47) (53.49, 46.30)|
+T Raene's Cleansing (3) |QID|1024| |N|Shael'dryn in Moonwell (53.49, 46.30)|
+A Raene's Cleansing (4) |QID|1026| |N|Shael'dryn in Moonwell (53.49, 46.30)|
 
-C An Aggressive Defense |QID|1025| |N|Kill 1 (npc:3746), 2 (npc:3749), 10 (npc:3750) and 12 (npc:3743) which can be found in the camp (51, 60)| |W| |NPC|3746, 3749, 3750, 3743|
+C An Aggressive Defense |QID|1025| |N|Kill 1 Foulweald Den Watcher, 2 Foulweald Ursa slain, 10 Foulweald Totemic, 12 Foulweald Warrior (51, 60)|
 
-R Astranaar |TID|1025| |N|Travel to {Astranaar} (36.59, 49.59)| |Z|1440|
-T An Aggressive Defense |QID|1025| |N|(npc:3691) in {Astranaar} (36.59, 49.59)| |Z|1440| |NPC|3691|
-T Elune's Tear |QID|1033| |N|(npc:3894) in {Astranaar} (37.37, 51.78)| |Z|1440| |NPC|3894|
-A The Ruins of Stardust |QID|1034| |N|(npc:3894) in {Astranaar} (37.37, 51.78)| |Z|1440| |NPC|3894|
+R Astranaar |TID|1025| |N|Travel to Astranaar (36.59, 49.59)|
+T An Aggressive Defense |QID|1025| |N|Raene Wolfrunner in Astranaar (36.59, 49.59)|
+T Elune's Tear |QID|1033| |N|Pelturas Whitemoon in Astranaar (37.37, 51.78)|
+A The Ruins of Stardust |QID|1034| |N|Pelturas Whitemoon in Astranaar (37.37, 51.78)|
 
-R Ruins of Stardust |QID|1034| |N|Travel to {Ruins of Stardust} (33.61, 67.49)|
-C The Ruins of Stardust |QID|1034| |N|Collect 5 (item:5494) which are found in Stardust covered bushes which are in the {Ruins of Stardust} (33.61, 67.49)| |W|
-N (item:5505) |TID|1034| |N|Destroy (item:5505) as it is no longer needed| |L|5505| |O|
-
-N Guide Complete |N|Tick to continue to the next guide| 
+R Ruins of Stardust |QID|1034| |N|Travel to Ruins of Stardust (33.61, 67.49)|
+C The Ruins of Stardust |QID|1034| |N|Collect 5 [Handful of Stardust] which are found in Stardust covered bushes which are in the Ruins of Stardust (33.61, 67.49)|
+N [Teronis' Journal] |TID|1034| |N|Destroy [Teronis' Journal] as it is no longer needed| |L|5505| |O|
 
 ]]
-end, {image = "ashenvale.tga", description = [[]]})	end
-	
-	function Guide:Unload()
-	end
-end
+end)
