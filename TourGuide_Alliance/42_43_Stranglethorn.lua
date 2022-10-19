fce12eaab30f72d0b5db86c348740da5b5683c59
kyliexx <kyliebuckler.fx@gmail.com>
2022-10-19 10:20:12 +0930

    edits

--- TourGuide_Alliance/42_43_Stranglethorn.lua
+++ TourGuide_Alliance/42_43_Stranglethorn.lua
@@ -3,30 +3,24 @@ TourGuide:RegisterGuide("Stranglethorn (42-43)", "Desolace (43-43)", "Alliance",
 return [[
 
 R Booty Bay |QID|2864| |N|Travel to Booty Bay (27.0, 77.2)|
-A Tran'rek |QID|2864| |N|Krazek in The Salty Sailor Tavern, in second floor (27.0, 77.2)|
-A Zanzil's Secret |QID|621| |N|Crank Fizzlebub in The Salty Sailor Tavern (27.1, 77.3)|
+A Tran'rek |QID|2864| |N|Krazek in The Salty Sailor Tavern, on second floor (27.0, 77.2)|
+A Zanzil's Secret |QID|621| |N|Crank Fizzlebub in The Salty Sailor Tavern, on ground floor (27.1, 77.3)|
 h Booty Bay |QID|617| |N|Speak to Innkeeper Skindle and set hearth at The Salty Sailor Tavern (27.1, 77.3)|
-A Akiris by the Bundle |QID|617| |N|Privateer Bloads in Booty Bay (26.7, 76.4)|
-A Scaring Shaky |QID|606| |N|\Sea Wolf\ MacKinley in Booty Bay (27.79, 77.08)|
+A Akiris by the Bundle |QID|617| |N|Privateer Bloads in Booty Bay, near the bank. (26.7, 76.4)|
 A The Bloodsail Buccaneers (Part 1) |QID|595| |N|First Mate Crazz in Booty Bay (28.08, 76.19)|
 T The Bloodsail Buccaneers (Part 3) |QID|599| |N|Fleet Master Seahorn in Booty Bay (27.1, 77.0)|
 
 T The Bloodsail Buccaneers (Part 1) |QID|595| |N|Bloodsail Correspondence in Southern Savage Coast, it's a parchment on top of a barrel (27.29, 69.51)| |OBJ|183|
 A The Bloodsail Buccaneers (Part 2) |QID|597| |N|Bloodsail Correspondence in Southern Savage Coast (27.29, 69.51)| |OBJ|183|
-C Scaring Shaky |QID|606| |N|Kill Elder Mistvale Gorilla and collect 5 Mistvale Giblets in Mistvale Valley (33.03, 65.66)|
 
 T The Bloodsail Buccaneers (Part 2) |QID|597| |N|First Mate Crazz in Booty Bay (28.08, 76.23)|
 A The Bloodsail Buccaneers (Part 3) |QID|599| |N|First Mate Crazz in Booty Bay (28.08, 76.23)|
 T The Bloodsail Buccaneers (Part 3) |QID|599| |N|Fleet Master Seahorn in Booty Bay (27.17, 76.98)|
 A The Bloodsail Buccaneers (Part 4) |QID|604| |N|Fleet Master Seahorn in Booty Bay (27.1, 77.0)|
-T Scaring Shaky |QID|606| |N|\Sea Wolf\ MacKinley in Booty Bay (27.79, 77.08)|
-A Return to MacKinley |QID|607| |N|\Sea Wolf\ MacKinley in Booty Bay (27.79, 77.08)|
-A Up to Snuff |QID|587| |N|Deeg in Booty Bay (27.0, 77.3)|
 T Singing Blue Shards |QID|605| |N|Crank Fizzlebub, in The Salty Sailor Tavern (27.11, 77.21)| |O|
 A Venture Company Mining |QID|600| |N|Crank Fizzlebub in The Salty Sailor Tavern (27.1, 77.3)| |PRE|605|
 T The Bloodsail Buccaneers (Part 3) |QID|599| |N|At Fleet Master Seahorn on the 2nd Floor of the Inn (27.1, 77)|
 A The Bloodsail Buccaneers (Part 4) |QID|604| |N|At Fleet Master Seahorn (27.1, 77)|
-T Return to MacKinley |QID|607| |N|\Sea Wolf\ MacKinley in Booty Bay (27.79, 77.08)|
 A Voodoo Dues |QID|609| |N|\Sea Wolf\ MacKinley in Booty Bay (27.79, 77.08)|
 A Stoley's Debt |QID|2872| |N|\Sea Wolf\ MacKinley in Booty Bay (27.8, 77.1)|
 A Keep An Eye Out |QID|576| |N|Dizzy One-Eye in Booty Bay (28.6, 75.8)|
@@ -42,22 +36,6 @@ R Booty Bay |TID|576| |N|Travel or Hearthstone to Booty Bay (27.1, 77.3)| |U|694
 T Up to Snuff |QID|587| |N|Deeg in Booty Bay (27.0, 77.3)|
 T The Bloodsail Buccaneers (Part 4) |QID|604| |N|Fleet Master Seahorn in Booty Bay (27.1, 77.0)|
 T Keep An Eye Out |QID|576| |N|Dizzy One-Eye in Booty Bay (28.6, 75.8)|
-
-N Green Hills of Stranglethorn - Page 1 |QID|339.1| |N|You should have most of the Chapters for 'The Green Hills of Stranglethorn' by now, but you still need Green Hills of Stranglethorn - Page 1. Try to purchase from the auction house Auctioneer O'reely in Booty Bay, sell extras or trade with other players in general chat. Tick this step to skip (26.6, 76.4)| |L|2725|
-N Green Hills of Stranglethorn - Page 4 |QID|339.2| |N|You should have most of the Chapters for 'The Green Hills of Stranglethorn' by now, but you still need Green Hills of Stranglethorn - Page 4. Try to purchase from the auction house Auctioneer O'reely in Booty Bay, sell extras or trade with other players in general chat. Tick this step to skip (26.6, 76.4)| |L|2728|
-N Green Hills of Stranglethorn - Page 6 |QID|339.3| |N|You should have most of the Chapters for 'The Green Hills of Stranglethorn' by now, but you still need Green Hills of Stranglethorn - Page 6. Try to purchase from the auction house Auctioneer O'reely in Booty Bay, sell extras or trade with other players in general chat. Tick this step to skip (26.6, 76.4)| |L|2730|
-N Green Hills of Stranglethorn - Page 8 |QID|339.4| |N|You should have most of the Chapters for 'The Green Hills of Stranglethorn' by now, but you still need Green Hills of Stranglethorn - Page 8. Try to purchase from the auction house Auctioneer O'reely in Booty Bay, sell extras or trade with other players in general chat. Tick this step to skip (26.6, 76.4)| |L|2732|
-N Green Hills of Stranglethorn - Page 10 |QID|340.1| |N|You should have most of the Chapters for 'The Green Hills of Stranglethorn' by now, but you still need Green Hills of Stranglethorn - Page 10. Try to purchase from the auction house Auctioneer O'reely in Booty Bay, sell extras or trade with other players in general chat. Tick this step to skip (26.6, 76.4)| |L|2734|
-N Green Hills of Stranglethorn - Page 11 |QID|340.2| |N|You should have most of the Chapters for 'The Green Hills of Stranglethorn' by now, but you still need Green Hills of Stranglethorn - Page 11. Try to purchase from the auction house Auctioneer O'reely in Booty Bay, sell extras or trade with other players in general chat. Tick this step to skip (26.6, 76.4)| |L|2735|
-N Green Hills of Stranglethorn - Page 14 |QID|340.3| |N|You should have most of the Chapters for 'The Green Hills of Stranglethorn' by now, but you still need Green Hills of Stranglethorn - Page 14. Try to purchase from the auction house Auctioneer O'reely in Booty Bay, sell extras or trade with other players in general chat. Tick this step to skip (26.6, 76.4)| |L|2738|
-N Green Hills of Stranglethorn - Page 16 |QID|340.4| |N|You should have most of the Chapters for 'The Green Hills of Stranglethorn' by now, but you still need Green Hills of Stranglethorn - Page 16. Try to purchase from the auction house Auctioneer O'reely in Booty Bay, sell extras or trade with other players in general chat. Tick this step to skip (26.6, 76.4)| |L|2740|
-N Green Hills of Stranglethorn - Page 18 |QID|341.1| |N|You should have most of the Chapters for 'The Green Hills of Stranglethorn' by now, but you still need Green Hills of Stranglethorn - Page 18. Try to purchase from the auction house Auctioneer O'reely in Booty Bay, sell extras or trade with other players in general chat. Tick this step to skip (26.6, 76.4)| |L|2742|
-N Green Hills of Stranglethorn - Page 20 |QID|341.2| |N|You should have most of the Chapters for 'The Green Hills of Stranglethorn' by now, but you still need Green Hills of Stranglethorn - Page 20. Try to purchase from the auction house Auctioneer O'reely in Booty Bay, sell extras or trade with other players in general chat. Tick this step to skip (26.6, 76.4)| |L|2744|
-N Green Hills of Stranglethorn - Page 21 |QID|341.3| |N|You should have most of the Chapters for 'The Green Hills of Stranglethorn' by now, but you still need Green Hills of Stranglethorn - Page 21. Try to purchase from the auction house Auctioneer O'reely in Booty Bay, sell extras or trade with other players in general chat. Tick this step to skip (26.6, 76.4)| |L|2745|
-N Green Hills of Stranglethorn - Page 24 |QID|341.4| |N|You should have most of the Chapters for 'The Green Hills of Stranglethorn' by now, but you still need Green Hills of Stranglethorn - Page 24. Try to purchase from the auction house Auctioneer O'reely in Booty Bay, sell extras or trade with other players in general chat. Tick this step to skip (26.6, 76.4)| |L|2748|
-N Green Hills of Stranglethorn - Page 25 |QID|342.1| |N|You should have most of the Chapters for 'The Green Hills of Stranglethorn' by now, but you still need Green Hills of Stranglethorn - Page 25. Try to purchase from the auction house Auctioneer O'reely in Booty Bay, sell extras or trade with other players in general chat. Tick this step to skip (26.6, 76.4)| |L|2749|
-N Green Hills of Stranglethorn - Page 26 |QID|342.2| |N|You should have most of the Chapters for 'The Green Hills of Stranglethorn' by now, but you still need Green Hills of Stranglethorn - Page 26. Try to purchase from the auction house Auctioneer O'reely in Booty Bay, sell extras or trade with other players in general chat. Tick this step to skip (26.6, 76.4)| |L|2750|
-N Green Hills of Stranglethorn - Page 27 |QID|342.3| |N|You should have most of the Chapters for 'The Green Hills of Stranglethorn' by now, but you still need Green Hills of Stranglethorn - Page 27. Try to purchase from the auction house Auctioneer O'reely in Booty Bay, sell extras or trade with other players in general chat. Tick this step to skip (26.6, 76.4)| |L|2751|
 
 R Southern Savage Coast |QID|617| |N|Travel to Southern Savage Coast (30.85, 72.37) (28.88, 58.10)| |Z|Stranglethorn Vale|
 C Akiris by the Bundle |QID|617| |N|Collect 10 Bundles of Akiris Reed from the Naga Explorer (24, 65)|
@@ -82,21 +60,6 @@ C Big Game Hunter |QID|208| |N|Kill King Bangalash and loot the Head of Bangalas
 
 R Nesingwary's Expedition |TID|208| |N|Travel to Nesingwary's Expedition (35.7, 10.8)| |PRE|197|
 T Big Game Hunter |QID|208| |N|Hemet Nesingwary in Nesingwary's Expedition (35.7, 10.8)| |PRE|197|
-A The Green Hills of Stranglethorn |QID|338| |N|Barnil Stonepot in Nesingwary's Expedition (35.65, 10.51)|
-A Chapter I |QID|339| |N|Barnil Stonepot in Nesingwary's Expedition (35.65, 10.51)|
-A Chapter II |QID|340| |N|Barnil Stonepot in Nesingwary's Expedition (35.65, 10.51)|
-A Chapter III |QID|341| |N|Barnil Stonepot in Nesingwary's Expedition (35.65, 10.51)|
-A Chapter IV |QID|342| |N|Barnil Stonepot in Nesingwary's Expedition (35.65, 10.51)|
-T Chapter I |QID|339| |N|Barnil Stonepot in Nesingwary's Expedition (35.65, 10.51)| |O|
-T Chapter II |QID|340| |N|Barnil Stonepot in Nesingwary's Expedition (35.65, 10.51)| |O|
-T Chapter III |QID|341| |N|Barnil Stonepot in Nesingwary's Expedition (35.65, 10.51)| |O|
-T Chapter IV |QID|342| |N|Barnil Stonepot in Nesingwary's Expedition (35.65, 10.51)| |O|
-T The Green Hills of Stranglethorn |QID|338| |N|Barnil Stonepot in Nesingwary's Expedition (35.65, 10.51)| |O|
-N Abandon Quest |QID|338| |N|Abandon The Green Hills of Stranglethorn and Chapter quest if you're unable to complete it now. You can complete this later| |O|
-N Abandon Quest |QID|339| |N|Abandon The Green Hills of Stranglethorn and Chapter quest if you're unable to complete it now. You can complete this later| |O|
-N Abandon Quest |QID|340| |N|Abandon The Green Hills of Stranglethorn and Chapter quest if you're unable to complete it now. You can complete this later| |O|
-N Abandon Quest |QID|341| |N|Abandon The Green Hills of Stranglethorn and Chapter quest if you're unable to complete it now. You can complete this later| |O|
-N Abandon Quest |QID|342| |N|Abandon The Green Hills of Stranglethorn and Chapter quest if you're unable to complete it now. You can complete this later| |O|
 
 R Booty Bay |TID|609| |N|Travel or Hearthstone to Booty Bay (27.1, 77.3)| |U|6948|
 T Zanzil's Secret |QID|621| |N|Crank Fizzlebub in The Salty Sailor Tavern (27.1, 77.3)|
@@ -110,6 +73,7 @@ T Keep An Eye Out |QID|576| |N|Dizzy One-Eye in Booty Bay (28.6, 75.8)|
 A Magical Analysis |QID|602| |N|Baron Revilgaz in Booty Bay (27.20, 76.90)|
 N Goblin Rumors |QID|1373| |N|Withdraw Goblin Rumors from the bank. Tick this step (26.57, 76.56)| |Z|Stranglethorn Vale| |L|5804| |OO|
 N Pristine Crawler Leg |QID|1258| |N|Withdraw Pristine Crawler Leg from the bank. Tick this step (26.57, 76.56)| |Z|Stranglethorn Vale| |L|5938| |OO|
+N Draenethyst Shard |QID|1373| |N|Withdraw Draenethyst Shard from the bank. Tick this step (26.57, 76.56)| |Z|Stranglethorn Vale| |L|6190| |OO|
 N Goblin Rumors |QID|1117| |N|Store Goblin Rumors to the bank (26.57, 76.56)| |Z|Stranglethorn Vale| |L|5804| |O|
 N Bag of Water Elemental Bracers |QID|602| |N|Store Bag of Water Elemental Bracers to the bank (26.57, 76.56)| |Z|Stranglethorn Vale| |L|3960| |O|
 N Encrusted Tail Fin |QID|1107| |N|Store Encrusted Tail Fin to the bank (26.57, 76.56)| |Z|Stranglethorn Vale| |L|5796| |O|
