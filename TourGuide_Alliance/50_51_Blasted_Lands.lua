6a926ae559082d4ba428892faac1cd403f833918
kyliebuckler <kyliebuckler.fx@gmail.com>
2024-07-13 18:46:41 +0930

    Expanded note and removed some unnecessary tags

--- TourGuide_Alliance/50_51_Blasted_Lands.lua
+++ TourGuide_Alliance/50_51_Blasted_Lands.lua
@@ -2,9 +2,9 @@ TourGuide:RegisterGuide("Blasted Lands (50-51)", "Un'goro (51-52)", "Alliance",f
 
 return [[
 
-R Nethergarde Keep |QID|2783| |N|Travel to Nethergarde Keep (66.91, 19.46)| |Z|Blasted Lands|
-T Thadius Grimshade |QID|2990| |N|Thadius Grimshade in Nethergarde Keep (66.91, 19.46)| |Z|Blasted Lands| |O|
-A Petty Squabbles |QID|2783| |N|Ambassador Ardalan in Nethergarde Keep (67.57, 19.29)| |Z|Blasted Lands|
+R Nethergarde Keep |QID|2783| |N|Travel to Nethergarde Keep (66.91, 19.46)|
+T Thadius Grimshade |QID|2990| |N|Thadius Grimshade in Nethergarde Keep (66.91, 19.46)| |O|
+A Petty Squabbles |QID|2783| |N|Ambassador Ardalan in Nethergarde Keep (67.57, 19.29)|
 
 R Swamp of Sorrows |QID|2801| |N|Travel to Swamp of Sorrows (34.24, 66.11)| |Z|Swamp of Sorrows|
 T Petty Squabbles |QID|2783| |N|Fallen Hero of the Horde in Swamp of Sorrows (34.24, 66.11)| |Z|Swamp of Sorrows|
@@ -12,28 +12,28 @@ A A Tale of Sorrow |QID|2801| |N|Fallen Hero of the Horde in Swamp of Sorrows (3
 C A Tale of Sorrow |QID|2801| |N|Speak to Fallen Hero of the Horde until the quest is complete (34.24, 66.11)| |Z|Swamp of Sorrows|
 T A Tale of Sorrow |QID|2801| |N|Fallen Hero of the Horde in Swamp of Sorrows (34.24, 66.11)| |Z|Swamp of Sorrows|
 
-N As you go... |AYG|2603| |QID|2603| |N|Kill creatures found all around Blasted Lands and collect the required materials below. The quest items will drop without needing to accept the quests, you can complete in any order|
-A The Basilisk's Bite |QID|2601| |N|Bloodmage Lynnore in Dreadmaul Hold (50.65, 14.27)| |Z|Blasted Lands|
-A Vulture's Vigor |QID|2603| |N|Bloodmage Lynnore in Dreadmaul Hold (50.59, 14.23)| |Z|Blasted Lands|
-A Snickerfang Jowls |QID|2581| |N|Bloodmage Drazial in Dreadmaul Hold (50.55, 14.21)| |Z|Blasted Lands|
-A A Boar's Vitality |QID|2583| |N|Bloodmage Drazial in Dreadmaul Hold (50.55, 14.21)| |Z|Blasted Lands|
-A The Decisive Striker |QID|2585| |N|Bloodmage Drazial in Dreadmaul Hold (50.55, 14.21)| |Z|Blasted Lands|
+N As you go... |AYG|2603| |QID|2603| |N|Kill creatures found all around Blasted Lands and collect the required materials from Vulture, Basilisk, Boar, Scorpion and Hyena type creatures. The quest items will drop without needing to accept the quests, you can complete in any order|
+A The Basilisk's Bite |QID|2601| |N|Bloodmage Lynnore in Dreadmaul Hold (50.65, 14.27)|
+A Vulture's Vigor |QID|2603| |N|Bloodmage Lynnore in Dreadmaul Hold (50.59, 14.23)|
+A Snickerfang Jowls |QID|2581| |N|Bloodmage Drazial in Dreadmaul Hold (50.55, 14.21)|
+A A Boar's Vitality |QID|2583| |N|Bloodmage Drazial in Dreadmaul Hold (50.55, 14.21)|
+A The Decisive Striker |QID|2585| |N|Bloodmage Drazial in Dreadmaul Hold (50.55, 14.21)|
 
-K Black Slayer |QID|2603| |N|Collect 14 Vulture Gizzard from Black Slayer, East of the road (53, 38) (59, 27)| |Z|Blasted Lands|  |L|8396 14|
-K Redstone Basilisk |QID|2601| |N|Collect 11 Basilisk Brain from Redstone Basilisk or Redstone Crystalhide, East of the road (60, 40) (57, 31)| |Z|Blasted Lands|  |L|8394 11|
-K Helboar |QID|2583| |N|Collect 6 Blasted Boar Lung from Helboar or Ashmane Boar, East of the road (52.5, 54) (57, 31)| |Z|Blasted Lands|  |L|8392 6|
-K Scorpok Stinger |QID|2585| |N|Collect 6 Scorpok Pincer from Scorpok Stinger, West of the road (47, 20)| |Z|Blasted Lands|  |L|8393 6|
-K Snickerfang Hyena |QID|2581| |N|Collect 5 Snickerfang Jowl from Snickerfang Hyena or Starving Snickerfang West of the road (47, 20) (50, 38)| |Z|Blasted Lands|  |L|8391 5|
-T The Basilisk's Bite |QID|2601| |N|Bloodmage Lynnore in Dreadmaul Hold (50.65, 14.27)| |Z|Blasted Lands|
-T Vulture's Vigor |QID|2603| |N|Bloodmage Lynnore in Dreadmaul Hold (50.59, 14.23)| |Z|Blasted Lands|
-T Snickerfang Jowls |QID|2581| |N|Bloodmage Drazial in Dreadmaul Hold (50.55, 14.21)| |Z|Blasted Lands|
-T A Boar's Vitality |QID|2583| |N|Bloodmage Drazial in Dreadmaul Hold (50.55, 14.21)| |Z|Blasted Lands|
-T The Decisive Striker |QID|2585| |N|Bloodmage Drazial in Dreadmaul Hold (50.55, 14.21)| |Z|Blasted Lands|
+K Black Slayer |QID|2603| |N|Collect 14 Vulture Gizzard from Black Slayer, East of the road (53, 38) (59, 27)|  |L|8396 14|
+K Redstone Basilisk |QID|2601| |N|Collect 11 Basilisk Brain from Redstone Basilisk or Redstone Crystalhide, East of the road (60, 40) (57, 31)|  |L|8394 11|
+K Helboar |QID|2583| |N|Collect 6 Blasted Boar Lung from Helboar or Ashmane Boar, East of the road (52.5, 54) (57, 31)|  |L|8392 6|
+K Scorpok Stinger |QID|2585| |N|Collect 6 Scorpok Pincer from Scorpok Stinger, West of the road (47, 20)|  |L|8393 6|
+K Snickerfang Hyena |QID|2581| |N|Collect 5 Snickerfang Jowl from Snickerfang Hyena or Starving Snickerfang West of the road (47, 20) (50, 38)|  |L|8391 5|
+T The Basilisk's Bite |QID|2601| |N|Bloodmage Lynnore in Dreadmaul Hold (50.65, 14.27)|
+T Vulture's Vigor |QID|2603| |N|Bloodmage Lynnore in Dreadmaul Hold (50.59, 14.23)|
+T Snickerfang Jowls |QID|2581| |N|Bloodmage Drazial in Dreadmaul Hold (50.55, 14.21)|
+T A Boar's Vitality |QID|2583| |N|Bloodmage Drazial in Dreadmaul Hold (50.55, 14.21)|
+T The Decisive Striker |QID|2585| |N|Bloodmage Drazial in Dreadmaul Hold (50.55, 14.21)|
 
-A Everything Counts In Large Amounts |QID|3501| |N|Kum'isha the Collector in Rise of the Defiler (51.99, 35.65)| |Z|Blasted Lands| |L|10593| |O|
-T Everything Counts In Large Amounts |QID|3501| |N|Kum'isha the Collector in Rise of the Defiler (51.99, 35.65)| |Z|Blasted Lands| |L|10593| |O|
-A To Serve Kum'isha |QID|2521| |N|Kum'isha the Collector in Rise of the Defiler (51.99, 35.65)| |Z|Blasted Lands| |L|8244| |O|
-T To Serve Kum'isha |QID|2521| |N|Kum'isha the Collector in Rise of the Defiler (51.99, 35.65)| |Z|Blasted Lands| |L|8244| |O|
+A Everything Counts In Large Amounts |QID|3501| |N|Kum'isha the Collector in Rise of the Defiler (51.99, 35.65)| |L|10593| |O|
+T Everything Counts In Large Amounts |QID|3501| |N|Kum'isha the Collector in Rise of the Defiler (51.99, 35.65)| |L|10593| |O|
+A To Serve Kum'isha |QID|2521| |N|Kum'isha the Collector in Rise of the Defiler (51.99, 35.65)| |L|8244| |O|
+T To Serve Kum'isha |QID|2521| |N|Kum'isha the Collector in Rise of the Defiler (51.99, 35.65)| |L|8244| |O|
 
 N Guide Complete |N|Tick to continue to the next guide|
 
