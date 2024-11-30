56292e443a9e9f677183eed3aec5ee1f42c81631
Vaniron <hrdlicka.luk@gmail.com>
2024-11-30 20:34:14 +0100

    Added Gilneas zone guide for the Horde (TurtleWoW content)

---
+++ TourGuide_Horde/42_43_Gilneas_Horde.lua
@@ -0,0 +1,88 @@
+TourGuide:RegisterGuide("Gilneas (42-43)", "Stranglethorn (42-43)", "Horde",function()
+
+return [[
+
+R Undercity |OID|40850| |N|Travel to Undercity (65.9, 42.6)| |Z|Undercity|
+A To Gilneas |QID|40850| |N|Dark Ranger Anya under Undercity Bank (65.9, 42.6)| |Z|Undercity|
+R The Greymane Wall |QID|40850| |N|Travel to The Greymane Wall in Silverpine Forest (47.2, 16.0)| |Z|Silverpine Forest|
+R Blackthorn's Camp |QID|40850| |N|Travel to Blackthorn's Camp in western Gilneas (14.5, 35.5)| |Z|Gilneas|
+T To Gilneas |QID|40850| |N|Blackthorn in Blackthorn's Camp (14.0, 33.7)|
+A Report to Livia Strongarm |QID|40846| |N|Blackthorn in Blackthorn's Camp (14.0, 33.7)| |Z|Gilneas|
+A Report to Luke Agamand |QID|40844| |N|Blackthorn in Blackthorn's Camp (14.0, 33.7)| |Z|Gilneas|
+T Report to Luke Agamand |QID|40844| |N|Luke Agamand in Blackthorn's Camp (14.1, 33.7)| |Z|Gilneas|
+A Heist in Dryrock Mine |QID|40845| |N|Luke Agamand in Blackthorn's Camp (14.1, 33.7)| |Z|Gilneas||PRE|40844|
+A Forsaken at the Church |QID|40851| |N|Luke Agamand in Blackthorn's Camp (14.1, 33.7)| |Z|Gilneas|
+N A Royal Heist |QID|41113| |N|Optional quest to be completed in Gilneas City dungeon| |Z|Gilneas|
+T Report to Livia Strongarm |QID|40846| |N|Livia Strongarmdd in Blackthorn's Camp (14.1, 34.3)| |Z|Gilneas|
+A Rendezvous with the Infiltrator |QID|40847| |N|Livia Strongarmdd in Blackthorn's Camp (14.1, 34.3)| |Z|Gilneas||PRE|40846|
+C Heist in Dryrock Mine |QID|40845| |N|Collect 16 Sacks of Mithril Ore from the mine uphill (21.2, 51.5)| |OBJ|25522|
+N High level elite! |QID|40847| |N|Watch out! There is high level elite near the inn where Greta is standing.| |Z|Gilneas|
+C Rendezvous with the Infiltrator |QID|40847| |N|Get the report from Greta south of Ruins of Greyshire (14.1, 34.3)| |Z|Gilneas| |OBJ|3023|
+A Greymane Hatred |QID|40980| |N|Harrison Blackheart south-east from Ruins of Greyshire (38.1, 60.8)| |Z|Gilneas|
+C Greymane Hatred |QID|40980| |N|Collect 40 Greymane Signets from soldiers to the west (19.9, 60.9)| |Z|Gilneas| |OBJ|1705|
+T Heist in Dryrock Mine |QID|40845| |N|Luke Agamand in Blackthorn's Camp (14.1, 33.7)| |Z|Gilneas|
+T Rendezvous with the Infiltrator |QID|40847| |N|Livia Strongarmdd in Blackthorn's Camp (14.1, 34.3)| |Z|Gilneas|
+A Quality Time with Blackthorn |QID|40848| |N|Blackthorn in Blackthorn's Camp (14.0, 33.7)| |Z|Gilneas||PRE|40847|
+C Quality Time with Blackthorn |QID|40848| |N|Kill 8 Spitecrest Netters on west shore (11.9, 39.0)| |Z|Gilneas|
+T Quality Time with Blackthorn |QID|40848| |N|Blackthorn in Blackthorn's Camp (14.0, 33.7)| |Z|Gilneas|
+N Genn Greymane Must Die! |QID|40849| |N|Optional quest to be completed in Gilneas City dungeon| |Z|Gilneas|
+T Greymane Hatred |QID|40980| |N|Harrison Blackheart south-east from Ruins of Greyshire (38.1, 60.8)| |Z|Gilneas|
+A The Blackheart Killer |QID|40981| |N|Harrison Blackheart south-east from Ruins of Greyshire (38.1, 60.8)| |Z|Gilneas||PRE|40980|
+R Stillward Church |OID|40850| |N|Travel north-east to the church (56.9, 38.1)| |Z|Gilneas|
+A Revenge After Death |QID|40974| |N|Dominic Larson in Stillward Church (56.6, 39.6)| |Z|Gilneas|
+A War on Worgen |QID|40976| |N|Deathstalker Vernon in Stillward Church (56.9, 39.4)| |Z|Gilneas|
+A A Chief Among Brutes |QID|40855| |N|Deathstalker Vernon in Stillward Church (56.9, 39.4)| |Z|Gilneas|
+A Vernon's Task |QID|40854| |N|Deathstalker Vernon in Stillward Church (56.9, 39.4)| |Z|Gilneas|
+A Dead Until Dark |QID|40877| |N|Deathstalker Alynna in Stillward Church (57.3, 39.6)| |Z|Gilneas|
+A Foulhide Mongrels |QID|40992| |N|Harlan Ballow in Stillward Church (57.5, 39.8)| |Z|Gilneas|
+T Forsaken at the Church |QID|40851| |N|Dark Bishop Mordren in Stillward Church (57.7, 39.6)| |Z|Gilneas|
+N For the Collection |QID|40991| |N|Optional quest to be completed in Badlands| |Z|Gilneas|
+N Through Greater Magic |QID|40993| |N|Optional quest to be completed in Desolace| |Z|Gilneas|
+h Stillward Church |QID|19159| |N|Speak to Dark Bishop Mordren and set hearthstone (57.7, 39.6)|
+f Stillward Church |QID|12569| |N|Speak to Andana and grab flight path for Stillward Church (57.1, 38.4)|
+R Ebonmere Farm |QID|40978| |N|Travel to Ebonmere Farm, north-west of Stillward Church (49.6, 31.1)| |Z|Gilneas|
+A Ebonmere Worgen Infestation |QID|40978| |N|Joshua Ebonmere in Ebonmere Farm (49.6, 31.1)| |Z|Gilneas|
+A Ebonmere Bat Infestation |QID|40977| |N|Joshua Ebonmere in Ebonmere Farm (49.6, 31.1)| |Z|Gilneas|
+C Ebonmere Bat Infestation |QID|40977| |N|Kill 12 bats around the farm| |Z|Gilneas|
+T Ebonmere Bat Infestation |QID|40977| |N|Joshua Ebonmere in Ebonmere Farm (49.6, 31.1)| |Z|Gilneas|
+C Revenge After Death |QID|40974| |N|Kill elite Snarlclaw in the cave west of the farm for his paw (41.1, 40.7)| |Z|Gilneas|
+C Ebonmere Worgen Infestation |QID|40978| |N|Kill worgens in the cave and in the north for their pelts (41.1, 40.7)| |Z|Gilneas|
+C War on Worgen |QID|40976| |N|Kill 9 alpha worgens in the cave (41.1, 40.7)| |Z|Gilneas|
+T Ebonmere Worgen Infestation |QID|40978| |N|Joshua Ebonmere in Ebonmere Farm (49.6, 31.1)| |Z|Gilneas|
+N Ebonmere Affairs |QID|40979| |N|Optional quest to be completed in Gilneas City dungeon| |Z|Gilneas|
+R Glaymore Stead |QID|40978| |N|Travel to Glaymore Stead, north of Stillward Church (53.4, 29.1)| |Z|Gilneas|
+N The Judge and the Phantom |QID|40975| |N|Optional quest to be completed in Gilneas City dungeon| |Z|Gilneas|
+R Brol'ok Mound |QID|40855| |N|Travel to Brol'ok Mound, south of Stillward Church (55.4, 61.4)| |Z|Gilneas|
+C A Chief Among Brutes |QID|40855| |N|Kill Chief Maulfist inside the cave for this crown (56.1, 55.2)| |Z|Gilneas|
+C Vernon's Task |QID|40854| |N|Kill ogres around for their heads (54.3, 61.8)| |Z|Gilneas|
+R Southmire Orchard |QID|40992| |N|Travel to Southmire Orchard, west of the cave (44.3, 66.1)| |Z|Gilneas|
+C Foulhide Mongrels |QID|40992| |N|Kill Foulhides for their Paws (44.3, 66.1)| |Z|Gilneas|
+R Ruins of Greyshire |QID|40877| |N|North-west of the orchard (34.5, 49.7)| |Z|Gilneas|
+T Dead Until Dark |QID|40877| |N|Orvan Darkeye in west Ruins of Greyshire (31.2, 47.0)| |Z|Gilneas|
+A All We Need is Blood |QID|40878| |N|Orvan Darkeye in west Ruins of Greyshire (31.2, 47.0)| |Z|Gilneas||PRE|40877|
+C All We Need is Blood |QID|40878| |N|Kill bats around| |Z|Gilneas|
+T All We Need is Blood |QID|40878| |N|Orvan Darkeye in west Ruins of Greyshire (31.2, 47.0)| |Z|Gilneas|
+A Last of the Living Dead |QID|40879| |N|Orvan Darkeye in west Ruins of Greyshire (31.2, 47.0)| |Z|Gilneas||PRE|40878|
+C Last of the Living Dead |QID|40879| |N|Kill undead around| |Z|Gilneas|
+T Last of the Living Dead |QID|40879| |N|Orvan Darkeye in west Ruins of Greyshire (31.2, 47.0)| |Z|Gilneas|
+A We Take It From The Living |QID|40880| |N|Orvan Darkeye in west Ruins of Greyshire (31.2, 47.0)| |Z|Gilneas||PRE|40879|
+R The Overgrown Acre |QID|40880| |N|Travel east to The Overgrown Acre (46.2, 60.1)| |Z|Gilneas|
+C We Take It From The Living |QID|40880| |N|Kill Bloodclaws around to get their blood| |Z|Gilneas|
+R Freyshear Keep |QID|40880| |N|Very small island on the south-east corner of the zone| |Z|Gilneas|
+C The Blackheart Killer |QID|40981| |N|Kill Captain Veller to get the necklace (66.7, 66.4)| |Z|Gilneas|
+C We Take It From The Living |QID|40880| |N|Kill Gilneans around to get their blood (65.2, 65.8)| |Z|Gilneas|
+T We Take It From The Living |QID|40880| |N|Orvan Darkeye in west Ruins of Greyshire (31.2, 47.0)| |Z|Gilneas|
+N The Evil Made Me Do It |QID|40881| |N|Optional quest to be completed in Gilneas City dungeon| |Z|Gilneas||PRE|40880|
+T The Blackheart Killer |QID|40981| |N|Harrison Blackheart south-east from Ruins of Greyshire (38.1, 60.8)| |Z|Gilneas|
+A In Memory of Franklin |QID|40982| |N|Harrison Blackheart south-east from Ruins of Greyshire (38.1, 60.8)| |Z|Gilneas||PRE|40981|
+T In Memory of Franklin |QID|40982| |N|Click the grave next to you, then speak with Harrison| |Z|Gilneas|
+T Revenge After Death |QID|40974| |N|Domindwic Larson in Stillward Church (56.6, 39.6)| |Z|Gilneas|
+T War on Worgen |QID|40976| |N|Deathstalker Vernon in Stillward Church (56.9, 39.4)| |Z|Gilneas|
+T A Chief Among Brutes |QID|40855| |N|Deathstalker Vernon in Stillward Church (56.9, 39.4)| |Z|Gilneas|
+T Vernon's Task |QID|40854| |N|Deathstalker Vernon in Stillward Church (56.9, 39.4)| |Z|Gilneas|
+T Foulhide Mongrels |QID|40992| |N|Harlan Ballow in Stillward Church (57.5, 39.8)| |Z|Gilneas|
+
+N Guide Complete |N|Tick to continue to the next guide|w
+]]
+
+end)
