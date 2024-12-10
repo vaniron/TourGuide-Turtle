13920df1dea1533988490ac048f2fc7eb71226ce
vaniron <hrdlicka.luk@gmail.com>
2024-12-10 22:25:00 +0100

    Winterspring rewrite (still not ideal)

--- TourGuide_Horde/54_55_Winterspring.lua
+++ TourGuide_Horde/54_55_Winterspring.lua
@@ -3,32 +3,27 @@ TourGuide:RegisterGuide("Winterspring (54-55)", "Felwood (55-56)", "Horde",funct
 
 return [[
 
-R Frostfire Hot Springs |QID|8464| |N|Travel to Frostfire Hot Springs (27.74, 34.53)| |Z|Winterspring| |REACH|
-T Speak to Salfa |QID|8465| |N|Salfa in Frostfire Hot Springs (27.74, 34.53)| |Z|Winterspring| |O| 
-A Winterfall Activity |QID|8464| |N|Salfa in Frostfire Hot Springs (27.74, 34.53)| |Z|Winterspring| 
-
-R Frostfire Hot Springs |QID|4842| |N|Travel to Frostfire Hot Springs (31.27, 45.17)| |Z|Winterspring| |REACH|
-T The New Springs |QID|980| |N|Donova Snowden in Frostfire Hot Springs (31.27, 45.17)| |Z|Winterspring| |O| 
-A Strange Sources |QID|4842| |N|Donova Snowden in Frostfire Hot Springs (31.27, 45.17)| |Z|Winterspring| |PRE|980| 
-A Threat of the Winterfall |QID|5082| |N|Donova Snowden in Frostfire Hot Springs (31.28, 45.17)| |Z|Winterspring| 
-T It's a Secret to Everybody (Part 3) |QID|3908| |N|Donova Snowden in Frostfire Hot Springs -Skip follow up (31.27, 45.17)| |Z|Winterspring| |O| 
-N Moontouched Feather |QID|978| |N|Collect Moontouched Feather from the ground near the quest giver (31.64, 44.83)| |Z|Winterspring|  |O| |OBJ|3651|
+R Winterspring |QID|8464| |N|Travel to Winterspring zone, far north of Orgrimmar.| |Z|Winterspring|
+T Speak to Salfa |QID|8465| |N|Salfa at zone entrance (27.74, 34.53)| |Z|Winterspring| |O|
+A Winterfall Activity |QID|8464| |N|Salfa in Frostfire Hot Springs (27.74, 34.53)| |Z|Winterspring|
 
 R Everlook |QID|3783| |N|Travel to Everlook (61.60, 38.62)| |Z|Winterspring| 
 T Felnok Steelspring |QID|4808| |N|Felnok Steelspring in Everlook (61.60, 38.62)| |Z|Winterspring| |O| 
-A Trouble in Winterspring! |QID|6603| |N|Meggi Peppinrocker in Everlook (61.23, 38.34)| |Z|Winterspring| 
-A Are We There, Yeti? |QID|3783| |N|Umi Rumplesnicker in Everlook (60.9, 37.6)| |Z|Winterspring| 
+A Trouble in Winterspring! |QID|6603| |N|Meggi Peppinrocker walking around in Everlook (61.23, 38.34)| |Z|Winterspring|
+A Are We There, Yeti? (part 1)|QID|3783| |N|Umi Rumplesnicker in Everlook (60.9, 37.6)| |Z|Winterspring|
 h Everlook |QID|5245| |N|Speak to Innkeeper Vizzie and set hearth in Everlook (61.36, 38.83)| |Z|Winterspring| 
 f Everlook |QID|3783| |N|Speak to Yugrek grab flight for Everlook (60.47, 36.30)| |Z|Winterspring| 
-N Blood Amber |QID|4102| |N|Store Blood Amber in the bank . Tick this step (61.4, 37.0)| |Z|Winterspring| |L|11503| |O| 
-N Felwood Slime Sample |QID|4661| |N|Store Felwood Slime Sample in the bank . Tick this step (61.4, 37.0)| |Z|Winterspring| |L|12230| |O| 
-N Everlook Report |QID|6029| |N|Store Everlook Report in the bank . Tick this step (61.4, 37.0)| |Z|Winterspring| |L|15788| |O| 
-N Studies in Spirit Speaking |QID|6030| |N|Store Studies in Spirit Speaking in the bank . Tick this step (61.4, 37.0)| |Z|Winterspring| |L|15790| |O| 
-N Deadwood Ritual Totem |QID|8470| |N|Store Studies in Spirit Speaking in the bank . Tick this step (61.4, 37.0)| |Z|Winterspring| |L|20741| |O| 
 
-C Strange Sources |QID|4842| |N|Go over the bridge and discover Darkwhisper Gorge (62.40, 67.17) (61.82, 70.63) (60.06, 73.49)| |Z|Winterspring| |PRE|980|
+R Frostfire Hot Springs |QID|4842| |N|Travel to Frostfire Hot Springs (31.27, 45.17)| |Z|Winterspring| |REACH|
+T The New Springs |QID|980| |N|Donova Snowden in Frostfire Hot Springs (31.27, 45.17)| |Z|Winterspring| |O|
+A Strange Sources |QID|4842| |N|Donova Snowden in Frostfire Hot Springs (31.27, 45.17)| |Z|Winterspring| |PRE|980|
+T Trouble in Winterspring! |QID|6603| |N|Donova Snowden in Frostfire Hot Springs (31.28, 45.17)| |Z|Winterspring|
+A Threat of the Winterfall |QID|5082| |N|Donova Snowden in Frostfire Hot Springs (31.28, 45.17)| |Z|Winterspring|
+T It's a Secret to Everybody (Part 3) |QID|3908| |N|Donova Snowden in Frostfire Hot Springs -Skip follow up (31.27, 45.17)| |Z|Winterspring| |O|
+N Moontouched Feather |QID|978| |N|Collect Moontouched Feather from the ground near the quest giver (31.64, 44.83)| |Z|Winterspring|  |O| |OBJ|3651|
+C Strange Sources |QID|4842| |N|Go to south-east of the zone and discover Darkwhisper Gorge (60.40, 73.37)| |Z|Winterspring| |PRE|980|
 
-R Winterfall Village |QID|8464| |N|Travel to Winterfall Village (67.13, 35.93)| |Z|Winterspring| 
+R Winterfall Village |QID|8464| |N|Travel north, east of Everlook to Winterfall Village (67.13, 35.93)| |Z|Winterspring|
 C Winterfall Activity |QID|8464| |N|Kill the required Winterfall fulborg in Winterfall Village (67.13, 35.93)| |Z|Winterspring|  
 C Threat of the Winterfall |QID|5082.2| |N|Kill 8 Winterfall Den Watcher in Winterfall Village (66.94, 34.82)| |Z|Winterspring| 
 N Empty Firewater Flask |QID|5083| |N|Collect Empty Firewater Flask from Winterfall fulborgs (67.13, 35.93)| |Z|Winterspring|  |L|12771| 
@@ -48,29 +43,29 @@ A Winterfall Firewater |QID|5083| |N|Use Empty Firewater Flask to accept quest| 
 
 R Frostfire Hot Springs |QID|5082| |N|Travel to Frostfire Hot Springs (31.28, 45.17)| |Z|Winterspring| |REACH|
 T Trouble in Winterspring! |QID|6603| |N|Donova Snowden in Frostfire Hot Springs (31.28, 45.17)| |Z|Winterspring| 
-T Strange Sources |QID|4842| |N|Donova Snowden in Frostfire Hot Springs (31.27, 45.17)| |Z|Winterspring| |PRE|980| 
+T Strange Sources |QID|4842| |N|Donova Snowden in Frostfire Hot Springs (31.27, 45.17)| |Z|Winterspring| |PRE|980|
+T Winterfall Firewater |QID|5083| |N|Donova Snowden in Frostfire Hot Springs (31.27, 45.17)| |Z|Winterspring|
 A Falling to Corruption |QID|5084| |N|Donova Snowden in Frostfire Hot Springs (31.27, 45.17)| |Z|Winterspring| 
-T Threat of the Winterfall |QID|5082| |N|Donova Snowden in Frostfire Hot Springs (31.28, 45.17)| |Z|Winterspring| 
-T Winterfall Firewater |QID|5083| |N|Donova Snowden in Frostfire Hot Springs (31.27, 45.17)| |Z|Winterspring| 
+T Threat of the Winterfall |QID|5082| |N|Donova Snowden in Frostfire Hot Springs (31.28, 45.17)| |Z|Winterspring|
 
 K Ragged Owlbeast |QID|4521.2| |N|Kill 15 Ragged Owlbeast in Winterspring follow the waypoint and kill as much as you can for now (38.77, 37.96) (45.77, 37.18) (49.78, 40.11)| |Z|Winterspring| |REACH| 
 
 R Ice Thistle Hills |QID|3783| |N|Travel or Hearthstone to Ice Thistle Hills (71.57, 39.08)| |Z|Winterspring|
-C Are We There, Yeti? |QID|3783| |N|Kill Ice Thistle Yeti or Ice Thistle Yeti and collect 10 Thick Yeti Fur in Ice Thistle Hills (71.57, 39.08)| |Z|Winterspring|  
+C Are We There, Yeti? (part 1)|QID|3783| |N|Kill Ice Thistle Yeti or Ice Thistle Yeti and collect 10 Thick Yeti Fur in Ice Thistle Hills (71.57, 39.08)| |Z|Winterspring|
 
-R Everlook |QID|977| |N|Travel to Everlook (60.89, 37.64)| |Z|Winterspring|
-T Are We There, Yeti? |QID|3783| |N|Umi Rumplesnicker in Everlook (60.89, 37.64)| |Z|Winterspring| 
-A Are We There, Yeti? |QID|977| |N|Umi Rumplesnicker in Everlook (60.9, 37.6)| |Z|Winterspring| 
+R Everlook |QID|3783| |N|Travel to Everlook (60.89, 37.64)| |Z|Winterspring|
+T Are We There, Yeti? (part 1)|QID|3783| |N|Umi Rumplesnicker in Everlook (60.89, 37.64)| |Z|Winterspring|
+A Are We There, Yeti? (part 2) |QID|977| |N|Umi Rumplesnicker in Everlook (60.9, 37.6)| |Z|Winterspring||PRE|3783|
 
 R Ice Thistle Hills |QID|977| |N|Travel to Ice Thistle Hills (68.88, 41.00)| |Z|Winterspring|
-C Are We There, Yeti? |QID|977| |N|Kill Ice Thistle Matriarch or Ice Thistle Patriarch and collect 2 Pristine Yeti Horn in Ice Thistle Hills (68.88, 41.00)| |Z|Winterspring|  
+C Are We There, Yeti? (part 2)|QID|977| |N|Kill Ice Thistle Matriarch or Ice Thistle Patriarch and collect 2 Pristine Yeti Horn in Ice Thistle Hills (68.88, 41.00)| |Z|Winterspring|
 
-R Everlook |QID|5163| |N|Travel to Everlook (60.89, 37.64)| |Z|Winterspring|
-T Are We There, Yeti? |QID|977| |N|Umi Rumplesnicker in Everlook (60.88, 37.63)| |Z|Winterspring| 
-A Are We There, Yeti? |QID|5163| |N|Umi Rumplesnicker in Everlook (60.9, 37.6)| |Z|Winterspring| 
+R Everlook |QID|977| |N|Travel to Everlook (60.89, 37.64)| |Z|Winterspring|
+T Are We There, Yeti? (part 2)|QID|977| |N|Umi Rumplesnicker in Everlook (60.88, 37.63)| |Z|Winterspring|
+A Are We There, Yeti? (part 3)|QID|5163| |N|Umi Rumplesnicker in Everlook (60.9, 37.6)| |Z|Winterspring||PRE|977|
 N Legacki |QID|5163.1| |N|Use Umi's Mechanical Yeti near Legacki in Everlook (61.53, 38.63)| |Z|Winterspring| |U|12928| 
-A The Everlook Report |QID|6029| |N|Gregor Greystone in Everlook (61.35, 38.95)| |Z|Winterspring| 
-A Duke Nicholas Zverenhoff |QID|6030| |N|Jessica Redpath in Everlook (61.28, 38.96)| |Z|Winterspring| 
+A The Everlook Report |QID|6029| |N|Gregor Greystone in Everlook inn (61.35, 38.95)| |Z|Winterspring|
+A Duke Nicholas Zverenhoff |QID|6030| |N|Gregor Greystone in Everlook inn (61.35, 38.95)| |Z|Winterspring|
 A Sister Pamela |QID|5601| |N|Jessica Redpath in Everlook (61.29, 38.89)| |Z|Winterspring| 
 N Umi's Mechanical Yeti |QID|5163| |N|Store Umi's Mechanical Yeti in the bank . Tick this step (61.4, 37.0)| |Z|Winterspring| |L|12928| |O| 
 N Winterfall Ritual Totem |QID|8471| |N|Store Winterfall Ritual Totem in the bank . Tick this step (61.4, 37.0)| |Z|Winterspring| |L|20742| |O| 
