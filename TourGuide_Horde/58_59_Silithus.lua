cd9b3f7e1115eaf9ef60f74e0503e51fe53958ab
VanironCZ <vanironcz@gmail.com>
2024-12-13 04:43:35 +0100

    1st part of Silithus tweaks

--- TourGuide_Horde/58_59_Silithus.lua
+++ TourGuide_Horde/58_59_Silithus.lua
@@ -13,40 +13,43 @@ A Rabine Saturna |QID|1123| |N|Arch Druid Hamuul Runetotem in Elder Rise (78.55,
 T A Donation of Mageweave |QID|7822| |N|Rumstag Proudstrider in Thunder Bluff (43.0, 42.7)| |Z|Thunder Bluff| |L|4338 60| |O| 
 T A Donation of Runecloth |QID|7823| |N|Rumstag Proudstrider in Thunder Bluff (43.0, 42.7)| |Z|Thunder Bluff| |L|14047 60| |O| 
 
-R Nighthaven |QID|1124| |N|Travel to Nighthaven in (51.72, 45.07)| |Z|Moonglade|
+R Nighthaven |QID|1124| |N|Travel to Nighthaven in Moonglade (northern Kalimdor) (51.72, 45.07)| |Z|Moonglade|
 T Rabine Saturna |QID|1123| |N|Rabine Saturna in Nighthaven (51.72, 45.07)| |Z|Moonglade| 
 A Wasteland |QID|1124| |N|Rabine Saturna in Nighthaven (51.72, 45.07)| |Z|Moonglade| 
 A A Reliquary of Purity |QID|5527| |N|Rabine Saturna in Nighthaven (51.72, 45.07)| |Z|Moonglade| |O| 
 
-R Camp Mojache |TID|4120| |N|Travel to Camp Mojache (76.17, 43.81)| |Z|Feralas| |O|
+R Camp Mojache |TID|4120| |N|Travel to Camp Mojache in Feralas (76.17, 43.81)| |Z|Feralas| |O|
 T The Strength of Corruption |QID|4120| |N|Talo Thornhoof in Camp Mojache (76.17, 43.81)| |Z|Feralas| |O| 
 
-R Gadgetzan |QID|5163| |N|Travel to Gadgetzan (51.07, 26.88)| |Z|Tanaris| |O|
-N Spitelash Siren |QID|5163.2| |N|Use Umi's Mechanical Yeti to scare Spitelash Siren in Gadgetzan (51.07, 26.88)| |Z|Tanaris| |O| |U|12928| 
+R Gadgetzan |QID|5163| |N|Travel to Gadgetzan in Tanaris (51.07, 26.88)| |Z|Tanaris| |O|
+N Are We There, Yeti? |QID|5163.2| |N|Use Umi's Mechanical Yeti to scare Sprinkle in Gadgetzan (51.07, 26.88)| |Z|Tanaris| |O| |Q|Are We There, Yeti?||QO|Scare Sprinkle: 1/1|
 
-R Marshal's Refuge |QID|5163| |N|Travel to Marshal's Refuge (43.66, 9.36)| |Z|Un'Goro Crater| |O|
-N Quixxil |QID|5163.3| |N|Use Umi's Mechanical Yeti to scare Quixxil in Marshal's Refuge (43.66, 9.36)| |Z|Un'Goro Crater| |O| |U|12928| 
+R Marshal's Refuge |QID|5163| |N|Travel to Marshal's Refuge in Un'Goro Crater (43.66, 9.36)| |Z|Un'Goro Crater| |O|
+N Are We There, Yeti? |QID|.3| |N|Use Umi's Mechanical Yeti to scare Quixxil in Marshal's Refuge (43.66, 9.36)| |Z|Un'Goro Crater| |O| |Q|Are We There, Yeti?||QO|Scare Quixxil: 1/1|
 C Melding of Influences |QID|4642| |N|Find a Primal Ooze and use Encased Corrupt Ooze and near it and it will become a Gargantuan Ooze, kill it and collect Merged Ooze Sample (39.06, 17.17)| |Z|Un'Goro Crater| |U|12288| 
 
 R Silithus |QID|1125| |N|Travel to Silithus (29.12, 22.11) (30.06, 10.41)| |Z|Silithus| |REACH|
+N Watch out! |QID|1125| |N|Watch out for Twilight elites patroling the roads.| |Z|Silithus|
 R Valor's Rest |QID|1125| |N|Travel to Valor's Rest in Silithus (81.83, 18.83)| |Z|Silithus|
 T Wasteland |QID|1124| |N|Layo Starstrike in Valor's Rest (81.83, 18.83)| |Z|Silithus| 
 A The Spirits of Southwind |QID|1125| |N|Layo Starstrike in Valor's Rest (81.79, 18.74)| |Z|Silithus| 
 
-R Southwind Village |QID|1125| |N|Travel to Southwind Village (60.22, 52.48)| |Z|Silithus| 
+R Southwind Village |QID|1125| |N|Travel south to Southwind Village (60.22, 52.48)| |Z|Silithus|
 C A Reliquary of Purity |QID|5527| |N|Collect Reliquary of Purity from Dusty Reliquary in Southwind Village (63.24, 55.31)| |Z|Silithus| |O| |OBJ|1387|
-C The Spirits of Southwind |QID|1125| |N|Kill Tortured Sentinel and 8 Tortured Druid in Southwind Village (62.46, 52.16)| |Z|Silithus|  
+C The Spirits of Southwind |QID|1125| |N|Kill Tortured Sentinel and 8 Tortured Druid in Southwind Village, adds can spawn after each kill (62.46, 52.16)| |Z|Silithus|
 T The Spirits of Southwind |QID|1125| |N|Layo Starstrike in Valor's Rest (81.87, 18.93)| |Z|Silithus| 
 
 R Valor's Rest |QID|1125| |N|Travel to Valor's Rest (81.87, 18.88)| |Z|Silithus|
 A Hive in the Tower |QID|1126| |N|Layo Starstrike in Valor's Rest (81.87, 18.93)| |Z|Silithus| 
 
 R Southwind Village |QID|1126| |N|Travel to Southwind Village (60.22, 52.48)| |Z|Silithus| 
-C Hive in the Tower |QID|1126| |N|Click on Hive' Ashi Pod on top of the tower and 2 Hive'Ashi Ambusher will spawn, kill them and collect Encrusted Silithid Object in Southwind Village (60.22, 52.48)| |Z|Silithus| |OBJ|5313| 
+C Hive in the Tower |QID|1126| |N|Click on Hive' Ashi Pod on top of the tower and Hive'Ashi Ambusher will spawn, kill it and collect Encrusted Silithid Object in Southwind Village (60.22, 52.48)| |Z|Silithus| |OBJ|5313|
 
 R Valor's Rest |QID|6844| |N|Travel to Valor's Rest (81.87, 18.88)| |Z|Silithus|
 T Hive in the Tower |QID|1126| |N|Layo Starstrike in Valor's Rest (81.87, 18.93)| |Z|Silithus| 
-A Umber, Archivist |QID|6844| |N|Layo Starstrike in Valor's Rest (81.87, 18.93)| |Z|Silithus| 
+A Umber, Archivist |QID|6844| |N|Layo Starstrike in Valor's Rest (81.87, 18.93)| |Z|Silithus|
+R Cenarion Hold |QID|6844| |N|Travel west to Cenarion Hold (53.31, 36.68)| |Z|Silithus|
+f Cenarion Hold |QID|6844| |N|Speak to Runk Windtamer and grab flight path for Cenarion Hold (48.71, 36.70)| |Z|Undercity|
 
 R Undercity |TID|4642| |N|Travel to Undercity (47.49, 73.40)| |Z|Undercity|
 N Extended Annals of Darrowshire |QID|5210| |N|Withdraw Extended Annals of Darrowshire from the bank (66.0, 45.2)| |Z|Undercity| |L|13202| |OO| 
