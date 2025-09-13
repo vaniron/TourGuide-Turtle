62b6763dac96f1a9fa43ffc9240401656dba9cd9
Trus3683 <152137641+Trus3683@users.noreply.github.com>
2025-09-13 12:05:53 -0700

    Update 01_12_Elwynn_Forest.lua

--- TourGuide_Alliance/01_12_Elwynn_Forest.lua
+++ TourGuide_Alliance/01_12_Elwynn_Forest.lua
@@ -224,7 +224,7 @@ A Continue to Stormwind |QID|6281| |N|Thor in Sentinel Hill (56.54, 52.64)| |Z|W
 F Sentinel Hill |QID|6261| |N|Speak to Thor and grab flight path for Sentinel Hill (56.54, 52.64)| |Z|Westfall|
 
 R Stormwind City |QID|6261| |N|Travel to Stormwind City (56.23, 64.59)| |Z|Stormwind City|
-T Shipment to Stormwind |QID|61| |N|Morgan Pestle in Trade District (56.23, 64.59)| |Z|Stormwind City|
+T Shipment to Stormwind |QID|61| |N|Morgan Pestle in Trade District (63.23, 74.59)| |Z|Stormwind City|
 T Continue to Stormwind |QID|6281| |N|Osric Strang in Old Town (74.21, 47.53)| |Z|Stormwind City|
 A Dungar Longdrink |QID|6261| |N|Osric Strang in Old Town (74.21, 47.53)| |Z|Stormwind City|
 T Dungar Longdrink |QID|6261| |N|Dungar Longdrink in Trade District (66.31, 62.19)| |Z|Stormwind City|
