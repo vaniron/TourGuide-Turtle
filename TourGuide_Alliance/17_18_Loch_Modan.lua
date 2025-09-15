1526a03d610940e5e2c101505e80326ff4c9d39b
Trus3683 <152137641+Trus3683@users.noreply.github.com>
2025-09-14 17:49:35 -0700

    Update 17_18_Loch_Modan.lua

--- TourGuide_Alliance/17_18_Loch_Modan.lua
+++ TourGuide_Alliance/17_18_Loch_Modan.lua
@@ -4,7 +4,7 @@ TourGuide:RegisterGuide("Loch Modan (17-18)", "Redridge Mountains (18-20)", "All
 return [[
 
 R Menethil Harbor |QID|307| |N|Travel to Menethil Harbor (9.6, 59.6)| |Z|Wetlands| |R|Night Elf|
-f Wetlands |QID|307| |N|Speak to Shellei Brondir and grab flight path for Menethil Harbor (9.6, 59.6)| |Z|Wetlands| |R|Night Elf|
+F Wetlands |QID|307| |N|Speak to Shellei Brondir and grab flight path for Menethil Harbor (9.6, 59.6)| |Z|Wetlands| |R|Night Elf|
 R Algaz Station |QID|307| |N|Follow the road to Algaz Station in Loch Modan (11.29, 53.52) (14.44, 45.60) (23.13, 43.43) (32.16, 39.21) (37.35, 36.79) (49.79, 39.21) (56.26, 52.75) (57.88, 61.70) (57.71, 69.11) (54.39, 72.04)| |Z|Wetlands| |R|Night Elf| |REACH|
 R Algaz Station |QID|307| |N|Travel to Algaz Station in Loch Modan (24.67, 18.25)|
 A Filthy Paws |QID|307| |N|Mountaineer Stormpike in Algaz Station (24.67, 18.25)|
@@ -13,10 +13,10 @@ C Filthy Paws |QID|307| |N|Go inside the cave to open the crates spread througho
 T Filthy Paws |QID|307| |N|Mountaineer Stormpike in Algaz Station (24.67, 18.25)|
 
 R Thelsamar |QID|436| |N|Travel to Thelsamar (33.9, 50.9)|
-f Thelsamar |QID|436| |N|Speak to Thorgrum Borrelson and grab flight point for Thelsamar (33.9, 50.9)|
+F Thelsamar |QID|436| |N|Speak to Thorgrum Borrelson and grab flight point for Thelsamar (33.9, 50.9)|
 T Rat Catching |Z|Loch Modan| |QID|416| |N|Mountaineer Kadrell in Thelsamar (34.94, 47.04)| |Z|Loch Modan|
 A Ironband's Excavation |QID|436| |N|Jern Hornhelm in Thelsamar (37.17, 47.55)|
-h Stoutlager Inn |TID|436| |N|Speak to Miran and set hearth at Stoutlager Inn (35.53, 48.41)|
+G Stoutlager Inn |TID|436| |N|Speak to Miran and set hearth at Stoutlager Inn (35.53, 48.41)|
 
 R Ironband's Excavation Site |QID|298| |N|Travel to Ironband's Excavation Site (64.83, 66.59)|
 T Ironband's Excavation |QID|436| |N|Magmar Fellhew in Ironband's Excavation Site (64.83, 66.59)|
@@ -35,14 +35,14 @@ C A Hunter's Challenge |QID|258| |N|Kill 5 Elder Mountain Boar found spread thro
 T A Hunter's Challenge |QID|258| |N|Daryl the Youngling in The Farstrider Lodge (83.43, 65.30)|
 A Vyrin's Revenge (Part 1) |QID|271| |N|Vyrin Swiftwind in The Farstrider Lodge (81.76, 64.15)|
 A Bingles' Missing Supplies |QID|2038| |N|Bingles Blastenheimer in The Loch (63.44, 47.78)|
-N As you go... |AYG|2038| |QID|385| |N|Kill Loch Crocolisk found in the the lake until you've collected 5 pieces of Crocolisk Meat and 6 Crocolisk Skin|
+N As you go... |AYG|2038| |QID|385| |N|Kill Loch Crocolisk found in the lake until you've collected 5 pieces of Crocolisk Meat and 6 Crocolisk Skin|
 N Bingles' Blastencapper |QID|2038.4| |N|Collect Bingles' Blastencapper from Bingles's Blastencapper on the ground (54.33, 26.54)|
 N Bingles' Hammer |QID|2038.3| |N|Collect Bingles' Hammer from the tool bucket in Loch Modan (51.79, 23.93)|
 N Bingles' Screwdriver |QID|2038.2| |N|Collect Bingles' Screwdriver from Bingle's Toolbucket in Loch Modan (48.35, 20.39)|
-N Bingles' Wrench |QID|2038.1| |N|Collect Bingles' Wrench from Bingle's Toolbucket near the camp fire in Loch Modan (48.69, 29.99)|
+N Bingles' Wrench |QID|2038.1| |N|Collect Bingles' Wrench from Bingle's Toolbucket near the campfire in Loch Modan (48.69, 29.99)|
 C In Defense of the King's Lands (Part 2) |QID|237| |N|Kill 10 Stonesplinter Seer and 10 Stonesplinter Skullthumper in Valley of Kings (48.35, 20.39)| |Z|Loch Modan|  |O|
 R Loch Modan |QID|2038| |N|Travel to Loch Modan|
-C Crocolisk Hunting |QID|385| |N|Kill Loch Crocolisk found in the the lake until you've collected 5 pieces of Crocolisk Meat and 6 Crocolisk Skin (57, 38)|
+C Crocolisk Hunting |QID|385| |N|Kill Loch Crocolisk found in the lake until you've collected 5 pieces of Crocolisk Meat and 6 Crocolisk Skin (57, 38)|
 
 R Stonewrought Dam |QID|250| |N|Travel to Stonewrought Dam (55.36, 14.91)|
 A A Dark Threat Looms (Part 1) |QID|250| |N|Chief Engineer Hinderweir VII in Stonewrought Dam (55.36, 14.91) (52.94, 10.99) (46.06, 13.72)|
@@ -53,10 +53,10 @@ T A Dark Threat Looms (Part 2) |QID|199| |N|Chief Engineer Hinderweir VII in Sto
 R Thelsamar |QID|301| |N|Travel to Thelsamar (42.60, 10.97) (37.17, 47.55)|
 T Excavation Progress Report |QID|298| |N|Jern Hornhelm in Thelsamar (37.17, 47.55)|
 A Report to Ironforge |QID|301| |N|Jern Hornhelm in Thelsamar (37.17, 47.55)|
-T Thelsamar Blood Sausages |QID|418| |N|in Stoutlager Inn (34.88, 49.18)| |Z|Loch Modan|
+T Thelsamar Blood Sausages |QID|418| |N|Vidra Hearthstove in Stoutlager Inn (34.88, 49.18)| |Z|Loch Modan|
 
 R Grizzlepaw Ridge |QID|271| |N|Travel to Grizzlepaw Ridge (39.98, 66.87)|
-C Vyrin's Revenge (Part 1) |QID|271| |N|Kill Ol' Sooty in the cave at the top of the mountain and collect Ol' Sooty's Head in Grizzlepaw Ridge<br/><b>Can be difficult to solo, it's safe to abandon and skip all Vyrin's Revenge quest (39.98, 66.87) (40.89, 63.06) (40.06, 61.55)|
+C Vyrin's Revenge (Part 1) |QID|271| |N|Kill Ol' Sooty in the cave at the top of the mountain and collect Ol' Sooty's Head in Grizzlepaw Ridge. This Can be difficult to solo; It's safe to abandon and skip all Vyrin's Revenge quests (39.98, 66.87) (40.89, 63.06) (40.06, 61.55)|
 
 R The Farstrider Lodge |QID|531| |N|Travel to The Farstrider Lodge (81.84, 61.77)|
 T Crocolisk Hunting |QID|385| |N|Marek Ironheart in The Farstrider Lodge (81.84, 61.77)|
@@ -71,7 +71,7 @@ T In Defense of the King's Lands (Part 2) |QID|237| |N|Mountaineer Gravelgaw in 
 R City of Ironforge |TID|301| |N|Travel to City of Ironforge (55.60, 47.94)| |Z|Ironforge|
 N Letter to Delgren |QID|967| |N|Store Letter to Delgren to the bank (35.48, 60.70)| |Z|Ironforge| |L|5354| |O|
 N Fandral's Message |QID|952| |N|Store Fandral's Message to the bank (35.48, 60.70)| |Z|Ironforge| |L|5390| |O|
-f The Great Forge |TID|301| |N|Speak to Gryth Thurden and grab flight path for The Great Forge (55.60, 47.94)| |Z|Ironforge|
+F The Great Forge |TID|301| |N|Speak to Gryth Thurden and grab flight path for The Great Forge (55.60, 47.94)| |Z|Ironforge|
 A Speak with Shoni |QID|2041| |N|Gnoarn in Ironforge (69.4, 50.6)| |Z|Ironforge|
 T Report to Ironforge |QID|301| |N|Prospector Stormpike in The Library (74.31, 12.16)| |Z|Ironforge|
 
