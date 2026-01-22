90d3837e3f1f3a97d9cd446696598b8207cc0de4
vaniron <hrdlicka.luk@gmail.com>
2026-01-22 10:10:59 +0100

    Added Blackstone Island guide

---
+++ TourGuide_Horde/01_12_BlackstoneIsland.lua
@@ -0,0 +1,111 @@
+TourGuide:RegisterGuide("Blackstone Island (1-12)", "The Barrens (12-20)", "Horde",function()
+
+return [[
+
+A Garbage Man |QID|80120| |N|Kazznik (36.51, 62.9)|
+A Pit-Iful Work |QID|41154| |N|Overseer Kizzcrack in the building on right (35.61, 63.01)|
+A Defunct Mainframes |QID|41150| |N|Balnack Copperlight outside under the tent (37.51, 62.81)|
+A Haywire Havoc |QID|41151| |N|Balnack Copperlight outside under the tent (37.51, 62.81)|
+A Foreman Pipelatch |QID|41153| |N|Foreman Pipelatch on the right in small hole (37.51, 67.91)|
+C Garbage Man |QID|80120| |N|Collect trash laying around and also coal for next quest (37.51, 67.91)|
+C Pit-Iful Work |QID|41154| |N|Collect coal from the piles, some of them are empty (36.31, 64.71)|
+C Defunct Mainframes |QID|41150| |N|Kill robots around for their mainframe (36.31, 64.71)|
+C Haywire Havoc |QID|41151| |N|Kill robotic chickens nearby (36.31, 64.71)|
+C Foreman Pipelatch |QID|41153| |N|Kill flying birds around (37.11, 71.41)|
+T Foreman Pipelatch |QID|41153| |N|Foreman Pipelatch on the right in small hole (37.51, 67.91)|
+T Defunct Mainframes |QID|41150| |N|Balnack Copperlight outside under the tent (37.51, 62.81)|
+T Haywire Havoc |QID|41151| |N|Balnack Copperlight outside under the tent (37.51, 62.81)|
+A DV-500 |QID|41152| |N|Balnack Copperlight outside under the tent (37.51, 62.81)|
+T Garbage Man |QID|80120| |N|Kazznik (36.51, 62.9)|
+T Pit-Iful Work |QID|41154| |N|Overseer Kizzcrack in the building on right (35.61, 63.01)|
+A Black Letter |QID|41206| |N|Overseer Kizzcrack (35.61, 63.01)| |C|Rogue|
+A Delayed Shipments |QID|41157| |N|Overseer Kizzcrack (35.61, 63.01)|
+A Foreman's Blues |QID|41156| |N|Overseer Kizzcrack (35.61, 63.01)|
+A The Robotics Expert |QID|41155| |N|Overseer Kizzcrack (35.61, 63.01)|
+T The Robotics Expert |QID|41155| |N|Balnack Copperlight outside under the tent (37.51, 62.81)|
+T Foreman's Blues |QID|41156| |N|Foreman Pipelatch on the right in small hole (37.51, 67.91)|
+T Delayed Shipments |QID|41157| |N|Dazlon Brassdigger (34.91, 78.11)|
+A Dirty Deeds Drowned Deep Below |QID|41159| |N|Dazlon Brassdigger (34.91, 78.11)|
+A Revenge of the Deep |QID|41158| |N|Dazlon Brassdigger (34.91, 78.11)|
+A Under The Sea |QID|41160| |N|Ranala Greedvolt (35.11, 78.11)|
+C Revenge of the Deep |QID|41158| |N|Kill crab-like creatures in the water and on the beach (33.01, 88.81)|
+C Under The Sea |QID|41160| |N|Collect gold from one of the Makruras (33.01, 88.81)|
+C Dirty Deeds Drowned Deep Below |QID|41159| |N|Loot the logbook from chest under the boat (31.01, 88.71)|
+T Revenge of the Deep |QID|41158| |N|Dazlon Brassdigger (34.91, 78.11)|
+T Dirty Deeds Drowned Deep Below |QID|41159| |N|Dazlon Brassdigger (34.91, 78.11)|
+T Under The Sea |QID|41160| |N|Collect gold from one of the Makruras (33.01, 88.81)|
+N Sell junk and train spells |QID|41152| |N|Sell all items you dont need and train your spells (49.71, 64.31)|
+A Mudpaw Mining Mishap |QID|41175| |N|Mining Foreman Bizzlo (49.61, 63.11)|
+A Disruption at Rustgate Lumber Yard |QID|41265| |N|Supervisor Ozzick (48.91, 64.21)|
+A Disruption at Black Ash Mine |QID|41264| |N|Supervisor Ozzick (48.91, 64.21)|
+A Workplace Hazard |QID|41174| |N|Technician Reyvo (48.91, 65.41)|
+A Redistribution |QID|41166| |N|Supplymaster Maxlo upstairs (47.51, 68.51)|
+A Salvaging Convoys |QID|41164| |N|Supplymaster Maxlo (47.51, 68.51)|
+A Dry As A Desert |QID|41165| |N|Supplymaster Maxlo (47.51, 68.51)|
+T Black Letter |QID|41206| |N|Leyti Quicktongue (48.51, 68.71)| |C|Rogue|
+A Maritime Gumbo |QID|41168| |N|Shazzlan inside (48.41, 69.31)|
+A Searching Wreckage |QID|41171| |N|Upstairs - Taxxlo Rustgate (48.21, 67.61)|
+A Bearer Of Bad News |QID|41169| |N|Taxxlo Rustgate (48.21, 67.61)|
+N Jump down |QID|41152| |N|On the lower roof and then on ground (48.31, 66.81)|
+A She Sells Sea Shells |QID|41176| |N|Schera Framesnap on the island in south-east (63.01, 77.71)|
+A Clearing Nuisances |QID|41162| |N|Fanzo Endgeline on one of the platforms (64.31, 83.11)|
+A The Supercutter 1500 |QID|41161| |N|Fanzo Endgeline (64.31, 83.11)|
+A The Murloc Threat |QID|41167| |N|Bruiser Rakklan in front of the building (62.01, 83.51)|
+A Battletron Calamity |QID|41163| |N|Gazzik upstairs on the balcony (61.31, 84.51)|
+C Maritime Gumbo |QID|41168| |N|Collect meat from the crabs and makrulas (39.31, 81.51)|
+T Maritime Gumbo |QID|41168| |N|Shazzlan inside (48.41, 69.31)|
+C Workplace Hazard |QID|41174| |N|Kill robots around (49.81, 52.01)|
+C Mudpaw Mining Mishap |QID|41175| |N|Kill Mudpaw miners for the supplies (52.11, 53.01)|
+C Battletron Calamity |QID|41163| |N|Kill faulty robots (44.31, 75.11)|
+C DV-500 |QID|41152| |N|Kill robot DV-500 for its CPU (42.41, 47.81)|
+C The Supercutter 1500 |QID|41161| |N|Get the remote from the chest upstairs on middle floor (44.11, 44.91)|
+T DV-500 |QID|41152| |N|Balnack Copperlight outside under the tent (37.51, 62.81)|
+C The Murloc Threat |QID|41167| |N|Kill murlocs for their scales (37.41, 37.91)|
+T Mudpaw Mining Mishap |QID|41175| |N|Mining Foreman Bizzlo (49.61, 63.11)|
+T Workplace Hazard |QID|41174| |N|Technician Reyvo (48.91, 65.41)|
+C Clearing Nuisances |QID|41162| |N|Kill big flying birds and scorpids (60.01, 48.01)|
+T The Murloc Threat |QID|41167| |N|Bruiser Rakklan in front of the building (62.01, 83.51)|
+T Battletron Calamity |QID|41163| |N|Gazzik upstairs on the balcony (61.31, 84.51)|
+T The Supercutter 1500 |QID|41161| |N|Fanzo Endgeline on one of the platforms (64.31, 83.11)|
+T Clearing Nuisances |QID|41162| |N|Fanzo Endgeline (64.31, 83.11)|
+C Redistribution |QID|41166| |N|Pick up the fireworks (61.41, 81.51)|
+C Disruption at Rustgate Lumber Yard |QID|41265| |N|Kill goblins (some are casting!) and ogres (58.51, 59.71)|
+C Salvaging Convoys |QID|41164| |N|Pick up buckets with tools, some of them are empty (56.91, 60.81)|
+T Disruption at Rustgate Lumber Yard |QID|41265| |N|Supervisor Ozzick (48.91, 64.21)|
+T Salvaging Convoys |QID|41164| |N|Supplymaster Maxlo upstairs (47.51, 68.51)|
+T Redistribution |QID|41166| |N|Supplymaster Maxlo (47.51, 68.51)|
+C Disruption at Black Ash Mine |QID|41264| |N|Enter the mine and kill the supervisor (53.51, 54.21)|
+A Ashen Pelts |QID|41183| |N|Pumpworker Fizzlo in far north (49.51, 24.81)|
+C Ashen Pelts |QID|41183| |N|Kill hyenas for their pelts (46.31, 36.71)|
+T Ashen Pelts |QID|41183| |N|Pumpworker Fizzlo (49.51, 24.81)|
+T Dry As A Desert |QID|41165| |N|Pumpmaster Galvax little bit to the east (58.51, 25.01)|
+A Right From Under My Green Nose |QID|41184| |N|Pumpmaster Galvax (58.51, 25.01)|
+A Grabb Mudhide |QID|41186| |N|Bruiser Yevo (57.71, 25.11)|
+A War On Mudpaw |QID|41185| |N|Bruiser Yevo (57.71, 25.11)|
+C She Sells Sea Shells |QID|41176| |N|Collect the sea shells from the ground  and from the gnolls (64.51, 19.71)|
+C Searching Wreckage |QID|41171| |N|Collect the book from the ship (76.01, 65.01)|
+T She Sells Sea Shells |QID|41176| |N|Schera Framesnap on the island in south-east (63.01, 77.71)|
+T Disruption at Black Ash Mine |QID|41264| |N|Supervisor Ozzick (48.91, 64.21)|
+A The Big Man |QID|41266| |N|Supervisor Ozzick (48.91, 64.21)|
+T The Big Man |QID|41266| |N|Upstairs - Taxxlo Rustgate (48.21, 67.61)|
+T Searching Wreckage |QID|41171| |N|Taxxlo Rustgate (48.21, 67.61)|
+A War on Venture Co. |QID|41172| |N|Taxxlo Rustgate (48.21, 67.61)|
+C War on Venture Co. |QID|41172| |N|Collect medailons from the goblins and ogres (60.91, 60.51)|
+C War On Mudpaw |QID|41185| |N|Collect the armbands from the gnolls (66.81, 51.11)|
+C Right From Under My Green Nose |QID|41184| |N|Collect the shipment (55.11, 33.41)|
+N Die on purpose |QID|41186| |N|And release at spirit healer to turn in all the quests (55.11, 33.41)|
+T Right From Under My Green Nose |QID|41184| |N|Supplymaster Maxlo upstairs (47.51, 68.51)|
+T War on Venture Co. |QID|41172| |N|Upstairs - Taxxlo Rustgate (48.21, 67.61)|
+A Destroying Venture Co. Leadership |QID|41173| |N|Taxxlo Rustgate (48.21, 67.61)|
+T War On Mudpaw |QID|41185| |N|Bruiser Yevo fart at the north edge (57.71, 25.11)|
+N Skip if too hard |QID|41173| |N|You can skip next two quests if too hard for you (57.71, 25.11)|
+C Grabb Mudhide |QID|41186| |N|Enter this cave and kill gnoll leader for his paw (64.41, 32.11)|
+C Destroying Venture Co. Leadership |QID|41173| |N|Kill both goblin leaders for their heads (54.21, 31.41)|
+T Grabb Mudhide |QID|41186| |N|Bruiser Yevo (57.71, 25.11)|
+T Destroying Venture Co. Leadership |QID|41173| |N|Taxxlo Rustgate fart at the south edge (48.21, 67.61)|
+N Bearer Of Bad News |QID|41169| |N|Exit the island by flying plane to the Durotar (63.81, 82.31)|
+
+N Guide Complete |N|Tick to continue to the next guide|
+
+]]
+end)
